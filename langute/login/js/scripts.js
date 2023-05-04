4188bb4c77186aada4c96e18fc4bdd0a0a0e1c8c
nargesafraz <nargesafraz1378@gmail.com>
2023-05-04 18:21:02 +0430

    delete login

--- langute/login/js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".dropdown").click(function(){
-        $(".menu").toggleClass("showMenu");
-        $(".menu > li").click(function(){
-            $(".dropdown > p").html($(this).html());
-            $(".menu").removeClass("showMenu");
-        });
-    });
-});
