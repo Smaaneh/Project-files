c6fae9b337ff6a55a4193d985016dd2098f2087d
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-06-26 16:11:31 +0430

    Update deleteTeacher.php

--- langute/AdminPanel/pages/teacher/deleteTeacher.php
+++ langute/AdminPanel/pages/teacher/deleteTeacher.php
@@ -5,4 +5,11 @@ $username = "root"; // نام کاربری پایگاه داده
 $password = "123"; // رمز عبور پایگاه داده
 $dbname = "langute"; // نام پایگاه داده
 
+// ایجاد اتصال به پایگاه داده
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// بررسی وضعیت اتصال
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
