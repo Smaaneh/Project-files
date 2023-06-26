4dcb053b840d21ae81e7a98fc358a5518b5937a9
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-06-26 16:18:10 +0430

    Update teacher.php

--- langute/AdminPanel/pages/teacher/teacher.php
+++ langute/AdminPanel/pages/teacher/teacher.php
@@ -208,116 +208,115 @@
 <!-- **************************************************** -->
 
 
+ 
   <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>مدیریت استادان</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-left">
-              <li class="breadcrumb-item"><a href="../HomeAdmin.html">خانه</a></li>
-              <li class="breadcrumb-item active">استادان</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>مدیریت استادان</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-left">
+                        <li class="breadcrumb-item"><a href="../HomeAdmin.html">خانه</a></li>
+                        <li class="breadcrumb-item active">استادان</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
     </section>
 
     <?php
-// اطلاعات اتصال به پایگاه داده
-$servername = "localhost"; // آدرس سرور پایگاه داده
-$username = "root"; // نام کاربری پایگاه داده
-$password = "123"; // رمز عبور پایگاه داده
-$dbname = "langute"; // نام پایگاه داده
-// ایجاد اتصال به پایگاه داده
-$conn = new mysqli($servername, $username, $password, $dbname);
+    // اطلاعات اتصال به پایگاه داده
+    $servername = "localhost"; // آدرس سرور پایگاه داده
+    $username = "root"; // نام کاربری پایگاه داده
+    $password = "123"; // رمز عبور پایگاه داده
+    $dbname = "langute"; // نام پایگاه داده
+    // ایجاد اتصال به پایگاه داده
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-// بررسی وضعیت اتصال
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    // بررسی وضعیت اتصال
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-// دریافت اطلاعات از جدول teacher
-$sql = "SELECT * FROM teacher";
-$result = $conn->query($sql);
+    // دریافت اطلاعات از جدول teacher
+    $sql = "SELECT * FROM teacher";
+    $result = $conn->query($sql);
 
-?>
+    ?>
 
-<!-- Main content -->
-<section class="content">
-  <div class="row">
-    <div class="col-12">
-      <div class="card">
-        <a class="btn btn-success btn-lg" href="addTeacher.php">افزودن استاد جدید +</a>
-        <div class="card-header">
-          <h3 class="card-title">اسامی استادان سایت لنگوته</h3>
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <a class="btn btn-success btn-lg" href="addTeacher.html">افزودن استاد جدید +</a>
+                    <div class="card-header">
+                        <h3 class="card-title">اسامی استادان سایت لنگوته</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>نام</th>
+                                    <th>نام خانوادگی</th>
+                                    <th>تخصص</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                // نمایش رکوردهای جدول teacher
+                                if ($result->num_rows > 0) {
+                                    while ($row = $result->fetch_assoc()) {
+                                        echo "<tr>";
+                                        echo "<td>" . $row['name'] . "</td>";
+                                        echo "<td>" . $row['last_name'] . "</td>";
+                                        echo "<td>" . $row['expertise'] . "</td>";
+                                        echo "<td><button type='button' class='btn btn-block btn-info btn-sm'>ویرایش</button></td>";
+                                        echo "<td><button type='button' class='btn btn-block btn-danger btn-sm' onclick='confirmDelete(" . $row['id'] . ")'>حذف</button></td>";
+                                        echo "</tr>";
+                                    }
+                                } else {
+                                    echo "<tr><td colspan='3'>هیچ رکوردی یافت نشد</td></tr>";
+                                }
+                                ?>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>نام</th>
+                                    <th>نام خانوادگی</th>
+                                    <th>تخصص</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
         </div>
-        <!-- /.card-header -->
-        <div class="card-body">
-          <table id="example1" class="table table-bordered table-striped">
-            <thead>
-              <tr>
-                <th>نام</th>
-                <th>نام خانوادگی</th>
-                <th>تخصص</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              // نمایش رکوردهای جدول teacher
-              if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                  echo "<tr>";
-                  echo "<td>" . $row['name'] . "</td>";
-                  echo "<td>" . $row['last_name'] . "</td>";
-                  echo "<td>" . $row['expertise'] . "</td>";
-                  echo "<td><button type='button' class='btn btn-block btn-info btn-sm'>ویرایش</button></td>";
-                  echo "<td><button type='button' class='btn btn-block btn-danger btn-sm' onclick='confirmDelete(" . $row['id'] . ")'>حذف</button></td>";
-                  echo "</tr>";
-                }
-              } else {
-                echo "<tr><td colspan='3'>هیچ رکوردی یافت نشد</td></tr>";
-              }
-              ?>
-            </tbody>
-            <tfoot>
-              <tr>
-                <th>نام</th>
-                <th>نام خانوادگی</th>
-                <th>تخصص</th>
-              </tr>
-            </tfoot>
-          </table>
-        </div>
-        <!-- /.card-body -->
-      </div>
-      <!-- /.card -->
-    </div>
-    <!-- /.col -->
-  </div>
-  <!-- /.row -->
-</section>
-<!-- /.content -->
+        <!-- /.row -->
+    </section>
+    <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
 
 <script>
-  function confirmDelete(id) {
+    function confirmDelete(id) {
     var result = confirm("آیا میخواهید این استاد حذف شود؟");
     if (result) {
-      // اگر کاربر تایید کرد، ارسال درخواست حذف به صفحه دیگر یا API
-      // مثال: window.location.href = "deleteTeacher.php?id=" + id;
-      console.log("رکورد با شناسه " + id + " حذف شد.");
+        // اگر کاربر تایید کرد، ارسال درخواست حذف به صفحه دیگر یا API
+        window.location.href = "deleteTeacher.php?id=" + id;
     } else {
-      console.log("حذف رکورد لغو شد.");
+        console.log("حذف رکورد لغو شد.");
     }
-  }
+}
 </script>
-
 
 
 
