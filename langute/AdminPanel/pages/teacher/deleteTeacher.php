a42f9887f9d179d4d6c934f4cb9d261ca01c7e97
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-06-26 16:12:42 +0430

    Update deleteTeacher.php

--- langute/AdminPanel/pages/teacher/deleteTeacher.php
+++ langute/AdminPanel/pages/teacher/deleteTeacher.php
@@ -13,3 +13,28 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// دریافت شناسه استاد برای حذف
+$id = $_GET['id'];
+
+// دریافت نام و نام خانوادگی استاد قبل از حذف
+$sql = "SELECT name, last_name FROM teacher WHERE id = $id";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $name = $row['name'];
+    $last_name = $row['last_name'];
+
+    // حذف رکورد مربوطه از جدول teacher
+    $sql = "DELETE FROM teacher WHERE id = $id";
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>alert('استاد $name $last_name حذف شد.')</script>";
+    } else {
+        echo "<script>alert('خطا در حذف رکورد.')</script>";
+    }
+} else {
+    echo "<script>alert('رکورد مورد نظر یافت نشد.')</script>";
+}
+
+// بستن اتصال به پایگاه داده
+$conn->close();
+?>
