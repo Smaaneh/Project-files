80af00cc66a5f406b286f6515e08a37aa810e02a
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-06-27 17:40:02 +0430

    Connect to database for editTeacher.php page

--- langute/AdminPanel/pages/teacher/editTeacher.php
+++ langute/AdminPanel/pages/teacher/editTeacher.php
@@ -204,6 +204,21 @@
   </aside>
 
 <!-- **************************************************** -->
+<?php
+// اطلاعات اتصال به پایگاه داده
+$servername = "localhost"; // آدرس سرور پایگاه داده
+$username = "root"; // نام کاربری پایگاه داده
+$password = "123"; // رمز عبور پایگاه داده
+$dbname = "langute"; // نام پایگاه داده
+
+// ایجاد اتصال به پایگاه داده
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// بررسی وضعیت اتصال
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 <!-- **************************************************** -->
 <footer class="main-footer">
 <div class="float-right d-none d-sm-block">
