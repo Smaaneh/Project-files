e9d25bf77eb8db38d3e6a3c135f1e61bd571be9b
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-06-28 00:24:03 +0430

    delete codss

--- langute/teachers.php
+++ langute/teachers.php
@@ -123,130 +123,70 @@
 		<!--/ End Breadcrumb -->
 
 		<!-- Teachers -->
-		<section class="teachers archive section">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-6 offset-lg-3 col-12">
-						<div class="section-title bg">
-							<h2>استادان <span>ما</span></h2>
-							<p>شما عزیزان میتوانید برای آشنایی بیشتر با استادان زبان سایت ما ، به تمام صفحات اجتماعی اساتید دسترسی داشته باشید </p>
-							<div class="icon"><i class="fa fa-users"></i></div>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-lg-4 col-md-6 col-12">
-						<!-- Single Teacher -->
-						<div class="single-teacher">
-							<div class="teacher-head overlay">
-								<img src="images/teachers/teacher-1.jpg" alt="#">
-								<ul class="social">
-									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-									<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-									<li><a href="#"><i class="fa fa-youtube"></i></a></li>
-								</ul>
-							</div>
-							<div class="teacher-content">
-								<h4>احسان محمدی<span>مدرس زبان انگلیسی و فرانسوی</span></h4>
-							</div>
-						</div>
-						<!--/ End Single Teacher -->
-					</div>
-					<div class="col-lg-4 col-md-6 col-12">
-						<!-- Single Teacher -->
-						<div class="single-teacher">
-							<div class="teacher-head overlay">
-								<img src="images/teachers/teacher-2.jpg" alt="#">
-								<ul class="social">
-									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-									<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-									<li><a href="#"><i class="fa fa-youtube"></i></a></li>
-								</ul>
-							</div>
-							<div class="teacher-content">
-								<h4>مهیار فلاح<span>مدرس زبان ایتالیایی</span></h4>
-							</div>
-						</div>
-						<!--/ End Single Teacher -->
-					</div>
-					<div class="col-lg-4 col-md-6 col-12">
-						<!-- Single Teacher -->
-						<div class="single-teacher">
-							<div class="teacher-head overlay">
-								<img src="images/teachers/teacher-3.jpg" alt="#">
-								<ul class="social">
-									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-									<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-									<li><a href="#"><i class="fa fa-youtube"></i></a></li>
-								</ul>
-							</div>
-							<div class="teacher-content">
-								<h4>نرگس افراز<span>مدرس زبان فرانسه</span></h4>
-							</div>
-						</div>
-						<!--/ End Single Teacher -->
-					</div>
-					<div class="col-lg-4 col-md-6 col-12">
-						<!-- Single Teacher -->
-							<div class="single-teacher">
-							<div class="teacher-head overlay">
-								<img src="images/teachers/teacher-4.jpg" alt="#">
-								<ul class="social">
-									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-									<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-									<li><a href="#"><i class="fa fa-youtube"></i></a></li>
-								</ul>
-							</div>
-							<div class="teacher-content">
-								<h4>الناز محمدی<span>مدرس زبان انگلیسی</span></h4>
-							</div>
-						</div>
-						<!--/ End Single Teacher -->
-					</div>
-					<div class="col-lg-4 col-md-6 col-12">
-						<!-- Single Teacher -->
-						<div class="single-teacher">
-							<div class="teacher-head overlay">
-								<img src="images/teachers/teacher-1.jpg" alt="#">
-								<ul class="social">
-									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-									<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-									<li><a href="#"><i class="fa fa-youtube"></i></a></li>
-								</ul>
-							</div>
-							<div class="teacher-content">
-								<h4>احسان محمدی<span>مدرس زبان انگلیسی و فرانسه</span></h4>
-							</div>
-						</div>
-						<!--/ End Single Teacher -->
-					</div>
-					<div class="col-lg-4 col-md-6 col-12">
-						<!-- Single Teacher -->
-						<div class="single-teacher">
-							<div class="teacher-head overlay">
-								<img src="images/teachers/teacher-2.jpg" alt="#">
-								<ul class="social">
-									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-									<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-									<li><a href="#"><i class="fa fa-youtube"></i></a></li>
-								</ul>
-							</div>
-							<div class="teacher-content">
-								<h4>مهیار فلاح<span>مدرس زبان ایتالیایی</span></h4>
-							</div>
-						</div>
-						<!--/ End Single Teacher -->
-					</div>
-				</div>
-			</div>
-		</section>
-		<!--/ End Teachers -->
+<section class="teachers archive section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 offset-lg-3 col-12">
+        <div class="section-title bg">
+          <h2>استادان <span>ما</span></h2>
+          <p>شما عزیزان میتوانید برای آشنایی بیشتر با استادان زبان سایت ما، به تمام صفحات اجتماعی اساتید دسترسی داشته باشید</p>
+          <div class="icon"><i class="fa fa-users"></i></div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <?php
+      // اتصال به دیتابیس
+      $servername = "localhost";
+      $username = "root";
+      $password = "123";
+      $dbname = "langute";
+
+      $conn = new mysqli($servername, $username, $password, $dbname);
+
+      if ($conn->connect_error) {
+        die("اتصال به دیتابیس با خطا مواجه شد: " . $conn->connect_error);
+      }
+
+      // دریافت اطلاعات استادان از جدول teacher
+      $sql = "SELECT * FROM teacher";
+      $result = $conn->query($sql);
+
+      if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+          $teacherName = $row["name"];
+          $teacherLastName = $row["last_name"];
+          $teacherExpertise = $row["expertise"];
+          $teacherPicture =  $row["Picture"];
+
+          // نمایش اطلاعات استاد در صفحه
+          echo '<div class="col-lg-4 col-md-6 col-12">';
+          echo '<div class="single-teacher">';
+          echo '<div class="teacher-head overlay">';
+          echo '<img src="' . $teacherPicture . '" alt="#">';
+          echo '<ul class="social">';
+          echo '<li><a href="#"><i class="fa fa-facebook"></i></a></li>';
+          echo '<li><a href="#"><i class="fa fa-twitter"></i></a></li>';
+          echo '<li><a href="#"><i class="fa fa-linkedin"></i></a></li>';
+          echo '<li><a href="#"><i class="fa fa-youtube"></i></a></li>';
+          echo '</ul>';
+          echo '</div>';
+          echo '<div class="teacher-content">';
+          echo '<h4>' . $teacherName . ' ' . $teacherLastName . '<span>' . $teacherExpertise . '</span></h4>';
+          echo '</div>';
+          echo '</div>';
+          echo '</div>';
+        }
+      } else {
+        echo "هیچ استادی یافت نشد.";
+      }
+
+      $conn->close();
+      ?>
+    </div>
+  </div>
+</section>
+<!--/ End Teachers -->
 
     <!-- Clients CSS -->
     <div class="clients">
