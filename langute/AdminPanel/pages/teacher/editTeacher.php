f5659a33b509ea7a3cb7fd960750fc5032f32c5d
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-06-27 17:38:13 +0430

    add foother for editTeacher.php

--- langute/AdminPanel/pages/teacher/editTeacher.php
+++ langute/AdminPanel/pages/teacher/editTeacher.php
@@ -204,3 +204,32 @@
   </aside>
 
 <!-- **************************************************** -->
+<!-- **************************************************** -->
+<footer class="main-footer">
+<div class="float-right d-none d-sm-block">
+ صفحه ادمین لنگوته
+</div>
+
+<strong>CopyLeft &copy; 2018<a href="http://github.com/smaaneh/">سمانه محمدی و نرگس افراز</a>.</strong>
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+  <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- FastClick -->
+<script src="../../plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+</body>
+</html>
