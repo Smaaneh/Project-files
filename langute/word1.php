4659ac847183925df117996d350e036084f64432
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-06-29 01:03:17 +0430

    connect to database

--- langute/word1.php
+++ langute/word1.php
@@ -139,6 +139,24 @@
 
   <!--/ End Slider Area -->
 <!-- main word -->
+<?php
+$servername = "localhost";  // نام سرور میزبان دیتابیس
+$username = "root";  // نام کاربری دیتابیس
+$password = "123";  // رمز عبور دیتابیس
+$dbname = "langute";  // نام دیتابیس
+
+// اتصال به دیتابیس
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// بررسی اتصال
+if ($conn->connect_error) {
+    die("اتصال به دیتابیس با مشکل مواجه شد: " . $conn->connect_error);
+}
+
+// کوئری برای دریافت اطلاعات
+$sql = "SELECT * FROM vocabulary WHERE lesson_id = 1";
+$result = $conn->query($sql);
+?>
 <div class="container mt-5">
   <div class="row">
     <div class="col-md-6 offset-md-3">
