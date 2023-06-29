9ec08b006faa940fd74d9dd1f21bd86dd7e6afd2
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-06-29 18:47:37 +0430

    Connecting to the database and reading the vocabulary table information

--- langute/AdminPanel/pages/cours/word/word.php
+++ langute/AdminPanel/pages/cours/word/word.php
@@ -203,6 +203,26 @@
     <!-- /.sidebar -->
   </aside>
 <!-- **************************************************** -->
+<?php
+// اطلاعات اتصال به پایگاه داده
+$servername = "localhost"; // آدرس سرور پایگاه داده
+$username = "root"; // نام کاربری پایگاه داده
+$password = "123"; // رمز عبور پایگاه داده
+$dbname = "langute"; // نام پایگاه داده
+
+// ایجاد اتصال به پایگاه داده
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// بررسی وضعیت اتصال
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// دریافت اطلاعات از جدول vocabulary
+$sql = "SELECT * FROM vocabulary";
+$result = $conn->query($sql);
+
+?>
 <!-- /.content-wrapper -->
 <!-- **************************************************** -->
 <footer class="main-footer">
