d11a8206c836e65dc73dc153ec45250950405851
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-06-29 18:48:17 +0430

    Update word.php

--- langute/AdminPanel/pages/cours/word/word.php
+++ langute/AdminPanel/pages/cours/word/word.php
@@ -223,6 +223,25 @@ $sql = "SELECT * FROM vocabulary";
 $result = $conn->query($sql);
 
 ?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>مدیریت واژگان</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-left">
+                        <li class="breadcrumb-item"><a href="../../HomeAdmin.html">خانه</a></li>
+                        <li class="breadcrumb-item active">واژگان</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
 <!-- /.content-wrapper -->
 <!-- **************************************************** -->
 <footer class="main-footer">
