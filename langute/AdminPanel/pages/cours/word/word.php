79d79ffc8a5ca7be0a77041508f7239d785fb01e
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-06-29 18:45:58 +0430

    add footer and update title

--- langute/AdminPanel/pages/cours/word/word.php
+++ langute/AdminPanel/pages/cours/word/word.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>افزودن کلمه جدید</title>
+  <title>مدیریت | واژگان</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -203,3 +203,27 @@
     <!-- /.sidebar -->
   </aside>
 <!-- **************************************************** -->
+<!-- /.content-wrapper -->
+<!-- **************************************************** -->
+<footer class="main-footer">
+<div class="float-right d-none d-sm-block">
+ صفحه ادمین لنگوته
+</div>
+
+<strong>CopyLeft &copy; 2018<a href="http://github.com/smaaneh/">سمانه محمدی و نرگس افراز</a>.</strong>
+</footer>
+
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="../../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- FastClick -->
+<script src="../../../plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../../dist/js/demo.js"></script>
+</body>
+</html>
