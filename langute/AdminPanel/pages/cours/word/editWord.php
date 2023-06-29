d92cfb2143a720ac34dbf58098773174fe12391e
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-06-29 19:03:35 +0430

    Content Header (Page header)

--- langute/AdminPanel/pages/cours/word/editWord.php
+++ langute/AdminPanel/pages/cours/word/editWord.php
@@ -278,6 +278,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // بستن اتصال به پایگاه داده
 $conn->close();
 ?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>ویرایش لغت</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-left">
+                        <li class="breadcrumb-item"><a href="../../HomeAdmin.html">خانه</a></li>
+                        <li class="breadcrumb-item"><a href="word.php">مدیریت واژگان</a></li>
+                        <li class="breadcrumb-item active">ویرایش لغت</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
 <!-- **************************************************** -->
 <footer class="main-footer">
 <div class="float-right d-none d-sm-block">
