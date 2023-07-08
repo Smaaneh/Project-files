b1e8d83297e93764a931a25c984c2d851264e1ae
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-08 14:29:53 +0430

    add grammer2

--- langute/grammer2.php
+++ langute/grammer2.php
@@ -10,7 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Title -->
-  <title>آموزش ها و درس ها</title>
+  <title>درس دو گرامر</title>
 
   <!-- Favicon -->
   <link rel="icon" type="image/png" href="images/favicon.png">
@@ -41,132 +41,141 @@
       <link rel="stylesheet" href="css/normalize.css">
       <link rel="stylesheet" href="style.css">
       <link rel="stylesheet" href="css/responsive.css">
-
+      <!-- Eduland film -->
+      <link rel="stylesheet" href="css/film.css">
   <!-- Eduland Colors -->
   <link rel="stylesheet" href="css/colors/color1.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <style>
-       .video-frame {
-           position: relative;
-           width: 640px;
-           height: 480px;
-           margin: 0 auto;
-           border: 20px solid #444;
-           background-color: #000;
-           overflow: hidden;
-       }
 
-       .tv-screen {
-           position: absolute;
-           top: 20px;
-           left: 20px;
-           width: 600px;
-           height: 440px;
-           background-color: #000;
-           overflow: hidden;
-       }
-
-       video {
-           width: 100%;
-           height: 100%;
-       }
-   </style>
 </head>
 <body>
-
   <!-- Header -->
   <header class="header">
-    <!-- Header Inner -->
-    <div class="header-inner overlay">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-3 col-md-3 col-12">
-            <!-- Logo -->
-            <div class="logo">
-              <a href="index.html"><img src="images/logo.png" alt="#"></a>
-            </div>
-            <!--/ End Logo -->
-            <div class="mobile-menu"></div>
-          </div>
-          <div class="col-lg-9 col-md-9 col-12">
-            <div class="menu-bar" >
-              <nav class="navbar navbar-default">
-                <div class="navbar-collapse">
-                  <!-- Main Menu -->
-                  <ul id="nav" class="nav menu navbar-nav">
-                    <li><a href="contact.html"><i class="fa fa-address-book"></i>ارتباط با ما</a> </li>
-                    <li><a href="events.html"><i class="fa fa-bullhorn"></i>مقالات</a></li>
-                    <li><a href="#"><i class="fa fa-clone"></i>صفحه ها</a>
-                      <ul class="dropdown">
-                        <li><a href="teachers.html">استادان</a></li>
-                                                  <li><a href="Social_learning.html">یادگیری اجتماعی</a></li>
-                                                  <li><a href="Test.html">آزمون</a></li>
-                                                  <li><a href="Progress_tracking.html">ردیابی پیشرفت</a></li>
-                      </ul>
-                    </li>
-                    <li><a href="Learning.html"><i class="fa fa-clone"></i>یادگیری با روش های مختلف</a></li>
-                    <li><a href="courses.html"><i class="fa fa-clone"></i>آموزش ها و درس ها</a></li>
-                    <li class="active"><a href="index.html"><i class="fa fa-home"></i>خانه</a></li>
-                  </ul>
-                  <!-- End Main Menu -->
+  			<!-- Header Inner -->
+  			<div class="header-inner overlay">
+  				<div class="container">
+  					<div class="row">
+  						<div class="col-lg-3 col-md-3 col-12">
+  							<!-- Logo -->
+  							<div class="logo">
+  								<a href="index.html"><img src="images/logo.png" alt="#"></a>
+  							</div>
+  							<!--/ End Logo -->
+  							<div class="mobile-menu"></div>
+  						</div>
+  						<div class="col-lg-9 col-md-9 col-12">
+  							<div class="menu-bar" >
+  								<nav class="navbar navbar-default">
+  									<div class="navbar-collapse">
+  										<!-- Main Menu -->
+  										<ul id="nav" class="nav menu navbar-nav">
+  											<li><a href="contact.html"><i class="fa fa-address-book"></i>ارتباط با ما</a> </li>
+  											<li><a href="events.html"><i class="fa fa-bullhorn"></i>مقالات</a></li>
+  											<li><a href="#"><i class="fa fa-clone"></i>صفحه ها</a>
+  												<ul class="dropdown">
+  													<li><a href="teachers.html">استادان</a></li>
+                                                      <li><a href="Social_learning.html">یادگیری اجتماعی</a></li>
+                                                      <li><a href="Test.html">آزمون</a></li>
+                                                      <li><a href="Progress_tracking.html">ردیابی پیشرفت</a></li>
+  												</ul>
+  											</li>
+  											<li><a href="Learning.html"><i class="fa fa-clone"></i>یادگیری با روش های مختلف</a></li>
+  											<li><a href="courses.html"><i class="fa fa-clone"></i>آموزش ها و درس ها</a></li>
+  											<li class="active"><a href="index.html"><i class="fa fa-home"></i>خانه</a></li>
+  										</ul>
+  										<!-- End Main Menu -->
+  									</div>
+  								</nav>
+  								<!-- Search Area -->
+  								<div class="search-area">
+  									<a href="#header" class="icon"><i class="fa fa-search"></i></a>
+  									<form class="search-form">
+  										<input type="text" placeholder="جستجو ..." name="search" style="direction:rtl;">
+  										<button value="search " type="submit"><i class="fa fa-search"></i></button>
+  									</form>
+  								</div>
+  								<!-- End Search Area-->
+  							</div>
+  						</div>
+  					</div>
+  				</div>
+  			</div>
+  			<!--/ End Header Inner -->
+  		</header>
+  		<!--/ End Header -->
+
+      <!-- Breadcrumb -->
+		<div class="breadcrumbs overlay" style="background-image:url('images/breadcrumb-bg.jpg')">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-12">
+						<h2>گرامر</h2>
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<ul class="bread-list">
+							<li><a href="index.html">خانه <i class="fa fa-angle-left"></i></a></li>
+							<li><a href="courses.html">آموزش و درس ها<i class="fa fa-angle-left"></i></a></li>
+              <li><a href="grammers.html">گرامر<i class="fa fa-angle-left"></i></a></li>
+							<li class="active"><a href="#">درس دو</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/ End Breadcrumb -->
+    <?php
+// اتصال به دیتابیس
+$servername = "localhost";
+$username = "root";
+$password = "123";
+$dbname = "langute";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// استعلام برای دریافت اطلاعات فیلم مورد نظر
+$sql = "SELECT Title, caption, Video FROM grammar WHERE lesson_id = 2";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // نمایش اطلاعات فیلم
+    while ($row = $result->fetch_assoc()) {
+        $title = $row["Title"];
+        $caption = $row["caption"];
+        $videoPath = $row["Video"];
+        ?>
+        <!-- caption & title -->
+        <section class="teachers archive section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 offset-lg-3 col-12">
+                        <div class="section-title bg">
+                            <h2><?php echo $title; ?></h2>
+                            <p><?php echo $caption; ?></p>
+                            <div class="icon"><i class="fa fa-users"></i></div>
+                        </div>
+                    </div>
                 </div>
-              </nav>
-              <!-- Search Area -->
-              <div class="search-area">
-                <a href="#header" class="icon"><i class="fa fa-search"></i></a>
-                <form class="search-form">
-                  <input type="text" placeholder="جستجو ..." name="search" style="direction:rtl;">
-                  <button value="search " type="submit"><i class="fa fa-search"></i></button>
-                </form>
-              </div>
-              <!-- End Search Area-->
+                <div class="video-frame">
+                    <div class="tv-screen">
+                        <video id="grammar-video" controls>
+                            <source src="<?php echo $videoPath; ?>" type="video/mp4">
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!--/ End Header Inner -->
-  </header>
-  <!--/ End Header -->
-  <div class="container">
-          <h1>English Grammar Video</h1>
-          <p>Welcome to our English grammar video lesson.</p>
+        </section>
+        <!--/ End cours -->
+        <?php
+    }
+} else {
+    echo "No videos found for lesson_id = 1";
+}
 
-          <div class="video-frame">
-              <div class="tv-screen">
-                  <video id="grammar-video" controls>
-                      <source src="video/dah.mp4" type="video/mp4">
-                      Your browser does not support the video tag.
-                  </video>
-              </div>
-          </div>
-      </div>
-
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-      <script>
-          var video = document.getElementById("grammar-video");
-          var playPauseBtn = document.getElementById("play-pause-btn");
-          var speedRange = document.getElementById("speed-range");
-          var volumeRange = document.getElementById("volume-range");
-
-          playPauseBtn.addEventListener("click", function() {
-              if (video.paused || video.ended) {
-                  video.play();
-                  playPauseBtn.textContent = "Pause";
-              } else {
-                  video.pause();
-                  playPauseBtn.textContent = "Play";
-              }
-          });
-
-          speedRange.addEventListener("input", function() {
-              video.playbackRate = speedRange.value;
-          });
-
-          volumeRange.addEventListener("input", function() {
-              video.volume = volumeRange.value;
-          });
-      </script>
+$conn->close();
+?>
 
     <!-- Clients CSS -->
     <div class="clients">
@@ -273,5 +282,7 @@
 		<script src="js/gmaps.min.js"></script>
 		<!-- Main JS-->
         <script src="js/main.js"></script>
+    <!-- course film JS-->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     </body>
 </html>
