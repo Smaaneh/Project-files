d0531b77880cf01eebc969249fab506b5cf2fe2f
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-11 18:44:57 +0430

    add fils and change names

--- langute/AdminPanel/pages/learning/songs/addsongs.php
+++ langute/AdminPanel/pages/learning/songs/addsongs.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>مدیریت | موسیقی</title>
+  <title>افزودن گفتار جدید</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -203,118 +203,163 @@
     <!-- /.sidebar -->
   </aside>
 <!-- **************************************************** -->
-<?php
-// اطلاعات اتصال به پایگاه داده
-$servername = "localhost"; // آدرس سرور پایگاه داده
-$username = "root"; // نام کاربری پایگاه داده
-$password = "123"; // رمز عبور پایگاه داده
-$dbname = "langute"; // نام پایگاه داده
-
-// ایجاد اتصال به پایگاه داده
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// بررسی وضعیت اتصال
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// دریافت اطلاعات از جدول songs
-$sql = "SELECT * FROM songs";
-$result = $conn->query($sql);
-
-?>
 <!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
+  <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>مدیریت موسیقی و پادکست</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-left">
-                        <li class="breadcrumb-item"><a href="../../HomeAdmin.html">خانه</a></li>
-                        <li class="breadcrumb-item active">موسیقی</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>افزودن گفتار</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-left">
+              <li class="breadcrumb-item"><a href="../HomeAdmin.html">خانه</a></li>
+              <li class="breadcrumb-item"><a href="speak.php">مدیریت گفتار</a></li>
+              <li class="breadcrumb-item active">افزودن گفتار</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
     </section>
-    <!-- Main content -->
-    <section class="content">
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <a class="btn btn-success btn-lg" href="addsongs.php">افزودن موسیقی جدید +</a>
+            <!-- left column -->
+            <div class="col-md-12">
+                <!-- general form elements -->
+                <div class="card card-success">
                     <div class="card-header">
-                        <h3 class="card-title">لیست کل موسیقی ها</h3>
+                        <h3 class="card-title">فرم افزودن گفتار</h3>
                     </div>
                     <!-- /.card-header -->
-                    <div class="card-body">
-                        <table id="example1" class="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>عنوان</th>
-                                    <th>کپشن</th>
-                                    <th>عملیات</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            <?php
-                                // نمایش رکوردهای جدول songs
-                                if ($result->num_rows > 0) {
-                                    while ($row = $result->fetch_assoc()) {
-                                        echo "<tr>";
-                                        echo "<td>" . $row['title'] . "</td>";
-                                        echo "<td>" . $row['caption'] . "</td>";
-                                        echo "<td><a href='editsongs.php?id=" . $row['id'] . "' class='btn btn-block btn-info btn-sm'>ویرایش</a></td>";
-                                        echo "<td><a href='deletesongs.php?id=" . $row['id'] . "' class='btn btn-block btn-danger btn-sm' onclick='confirmDelete(" . $row['id'] . ");'>حذف</a></td>";
-                                        echo "</tr>";
-                                    }
-                                } else {
-                                    echo "<tr><td colspan='4'>هیچ رکوردی یافت نشد</td></tr>";
-                                }
-                                ?>
-                            </tbody>
-                            <tfoot>
-                                <tr>
-                                   <th>عنوان</th>
-                                    <th>کپشن</th>
-                                    <th>عملیات</th>
-                                </tr>
-                            </tfoot>
-                        </table>
-</div>
-                    <!-- /.card-body -->
+                    <!-- form start -->
+                    <form role="form" method="post" enctype="multipart/form-data">
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="Title">عنوان</label>
+                                <input type="text" class="form-control" id="Title" name="Title"
+                                    placeholder="عنوان خود را وارد کنید">
+                            </div>
+                            <div class="form-group">
+                                <label for="caption">کپشن</label>
+                                <input type="text" class="form-control" id="caption" name="caption"
+                                    placeholder="کپشن را وارد کنید">
+                            </div>
+                            <!-- select -->
+                            <div class="form-group">
+                                <label>درس مد نظر خود را انتخاب کنید</label>
+                                <select class="form-control" name="cours">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                </select>
+                            </div>
+                            <!-- upload video -->
+                            <div class="form-group">
+                                <label for="video">آپلود ویدیو</label>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="video" name="video">
+                                        <label class="custom-file-label" for="video">فیلم مناسب گفتار</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text" id="">Upload</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer text-center">
+                            <button type="submit" class="btn btn-success" name="submit">ثبت</button>
+                        </div>
+                    </form>
                 </div>
                 <!-- /.card -->
             </div>
-            <!-- /.col -->
+            <!-- /.card-body -->
         </div>
-        <!-- /.row -->
-    </section>
-    <!-- /.content -->
+        <!-- /.card -->
+    </div>
 </div>
-<!-- /.content-wrapper -->
-<script>
-    function confirmDelete(id) {
-        var result = confirm("آیا میخواهید این موسیقی حذف شود؟");
-        if (result) {
-            // اگر کاربر تایید کرد، ارسال درخواست حذف به صفحه deletesongs.php
-            window.location.href = "deletesongs.php?id=" + id;
-        } else {
-            console.log("حذف رکورد لغو شد.");
-        }
-    }
-</script>
+<!-- /.card-body -->
+</section>
+<!-- /.content -->
+</body>
+
+</html>
 
 <?php
-// بستن اتصال به پایگاه داده
+$servername = "localhost";
+$username = "root";
+$password = "123";
+$dbname = "langute";
+// اتصال به دیتابیس
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// بررسی اتصال
+if ($conn->connect_error) {
+    die("خطا در اتصال به دیتابیس: " . $conn->connect_error);
+}
+
+// دریافت داده‌ها از فرم و ذخیره در دیتابیس
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // بررسی پر بودن تمام فیلدها
+  if (
+      empty($_POST["Title"]) ||
+      empty($_POST["caption"]) ||
+      empty($_POST["cours"]) ||
+      empty($_FILES["video"]["name"])
+  ) {
+      echo '<div class="alert alert-warning text-center mb-3">لطفاً تمام فیلدها را پر کنید و یک فایل ویدیو انتخاب کنید.</div>';
+  } else {
+      $Title = $_POST["Title"];
+      $caption = $_POST["caption"];
+      $cours = $_POST["cours"];
+      $video = $_FILES["video"]["name"];
+
+      // محدودیت‌های مربوط به ویدیو
+      $targetDir = "../../../../videos/uploads/speak/";
+      $targetFile = $targetDir . basename($_FILES["video"]["name"]);
+      $videoFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+      $maxFileSize = 500 * 1024 * 1024; // حداکثر سایز ویدیو: 500
+
+      // بررسی پسوند ویدیو
+      $allowedExtensions = array("mp4", "avi", "mkv");
+      if (!in_array($videoFileType, $allowedExtensions)) {
+          echo '<div class="alert alert-danger text-center mb-3">فقط فایل‌های با پسوند MP4، AVI و MKV مجاز هستند.</div>';
+      }
+      // بررسی سایز ویدیو
+      elseif ($_FILES["video"]["size"] > $maxFileSize) {
+          echo '<div class="alert alert-danger text-center mb-3">سایز فایل ویدیو باید کمتر از 100MB باشد.</div>';
+      }
+      // آپلود ویدیو
+      else {
+          if (move_uploaded_file($_FILES["video"]["tmp_name"], $targetFile)) {
+              // استفاده از prepared statement برای جلوگیری از حمله‌های اینجکشن
+              $stmt = $conn->prepare("INSERT INTO speak (Title, caption, lesson_id, video) VALUES (?, ?, ?, ?)");
+              $stmt->bind_param("ssss", $Title, $caption, $cours, $targetFile);
+
+              if ($stmt->execute()) {
+                  echo '<div class="alert alert-success text-center mb-3">اطلاعات با موفقیت ذخیره شدند.</div>';
+              } else {
+                  echo '<div class="alert alert-danger text-center mb-3">خطا در ذخیره اطلاعات: ' . $stmt->error . '</div>';
+              }
+
+              $stmt->close();
+          } else {
+              echo '<div class="alert alert-danger text-center mb-3">خطا در آپلود ویدیو.</div>';
+          }
+      }
+  }
+}
+
 $conn->close();
 ?>
-
-<!-- **************************************************** -->
+</div>
+<!-- /.content-wrapper -->
 <footer class="main-footer">
 <div class="float-right d-none d-sm-block">
  صفحه ادمین لنگوته
--- langute/AdminPanel/pages/learning/songs/editsongs.php
+++ langute/AdminPanel/pages/learning/songs/editsongs.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>مدیریت | موسیقی</title>
+  <title>مدیریت | واژگان</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -218,75 +218,106 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// دریافت اطلاعات از جدول songs
-$sql = "SELECT * FROM songs";
+// دریافت شناسه کلمه برای ویرایش
+$id = $_GET['id'];
+// دریافت اطلاعات کلمه قبل از ویرایش
+$sql = "SELECT * FROM speak WHERE id = $id";
 $result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $Title = $row['Title'];
+    $caption = $row['caption'];
+    $lesson_id = $row['lesson_id'];
+    $video = $row['Video'];
+} else {
+    echo "<script>alert('رکورد مورد نظر یافت نشد.')</script>";
+    exit();
+}
 
+// در صورتی که فرم ویرایش ارسال شده باشد
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // دریافت اطلاعات از فرم
+    $new_Title = $_POST['Title'];
+    $new_caption = $_POST['caption'];
+    $new_lesson_id = $_POST['lesson_id'];
+    $new_video = $_POST['video'];
+
+    // به روزرسانی رکورد کلمه در جدول speak
+    $update_sql = "UPDATE speak SET Title = '$new_Title', caption = '$new_caption', lesson_id = '$new_lesson_id', Video = '$new_video' WHERE id = $id";
+
+    if ($conn->query($update_sql) === TRUE) {
+        echo "<script>alert('ویرایش گفتار با موفقیت انجام شد.')</script>";
+        echo "<script>window.location.href = 'speak.php';</script>";
+    } else {
+        echo "<script>alert('خطا در ویرایش رکورد.')</script>";
+    }
+}
+
+// بستن اتصال به پایگاه داده
+$conn->close();
 ?>
 <!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
+<div class="content-wrapper
+
+">
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>مدیریت موسیقی و پادکست</h1>
+                    <h1>ویرایش گفتار</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-left">
                         <li class="breadcrumb-item"><a href="../../HomeAdmin.html">خانه</a></li>
-                        <li class="breadcrumb-item active">موسیقی</li>
+                        <li class="breadcrumb-item"><a href="speak.php">مدیریت گفتار</a></li>
+                        <li class="breadcrumb-item active">ویرایش گفتار</li>
                     </ol>
                 </div>
             </div>
         </div><!-- /.container-fluid -->
-    </section>
-    <!-- Main content -->
+    </section><!-- Main content -->
     <section class="content">
         <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <a class="btn btn-success btn-lg" href="addsongs.php">افزودن موسیقی جدید +</a>
+            <div class="col-md-12">
+                <div class="card card-primary">
                     <div class="card-header">
-                        <h3 class="card-title">لیست کل موسیقی ها</h3>
+                        <h3 class="card-title">فرم ویرایش گفتار</h3>
                     </div>
                     <!-- /.card-header -->
-                    <div class="card-body">
-                        <table id="example1" class="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>عنوان</th>
-                                    <th>کپشن</th>
-                                    <th>عملیات</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            <?php
-                                // نمایش رکوردهای جدول songs
-                                if ($result->num_rows > 0) {
-                                    while ($row = $result->fetch_assoc()) {
-                                        echo "<tr>";
-                                        echo "<td>" . $row['title'] . "</td>";
-                                        echo "<td>" . $row['caption'] . "</td>";
-                                        echo "<td><a href='editsongs.php?id=" . $row['id'] . "' class='btn btn-block btn-info btn-sm'>ویرایش</a></td>";
-                                        echo "<td><a href='deletesongs.php?id=" . $row['id'] . "' class='btn btn-block btn-danger btn-sm' onclick='confirmDelete(" . $row['id'] . ");'>حذف</a></td>";
-                                        echo "</tr>";
-                                    }
-                                } else {
-                                    echo "<tr><td colspan='4'>هیچ رکوردی یافت نشد</td></tr>";
-                                }
-                                ?>
-                            </tbody>
-                            <tfoot>
-                                <tr>
-                                   <th>عنوان</th>
-                                    <th>کپشن</th>
-                                    <th>عملیات</th>
-                                </tr>
-                            </tfoot>
-                        </table>
-</div>
-                    <!-- /.card-body -->
+                    <!-- form start -->
+                    <form role="form" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) . '?id=' . $id; ?>">
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="Title">عنوان</label>
+                                <input type="text" class="form-control" id="Title" name="Title" value="<?php echo $Title; ?>" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="caption">کپشن</label>
+                                <input type="text" class="form-control" id="caption" name="caption" value="<?php echo $caption; ?>" required>
+                            </div>
+                            <!-- select -->
+                            <div class="form-group">
+                                <label>درس مد نظر خود را انتخاب کنید</label>
+                                <select class="form-control" name="lesson_id">
+                                <option value="1" <?php if ($lesson_id == 1) echo 'selected'; ?>>1</option>
+                                    <option value="2" <?php if ($lesson_id == 2) echo 'selected'; ?>>2</option>
+                                    <option value="3" <?php if ($lesson_id == 3) echo 'selected'; ?>>3</option>
+                                    <option value="4" <?php if ($lesson_id == 4) echo 'selected'; ?>>4</option>
+                                </select>
+                            </div>
+                            <!-- /select -->
+                            <div class="form-group">
+                                <label for="video">فیلم</label>
+                                <input type="file" class="form-control" id="video" name="video">
+                            </div>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">ثبت تغییرات</button>
+                        </div>
+                    </form>
                 </div>
                 <!-- /.card -->
             </div>
@@ -297,24 +328,7 @@ $result = $conn->query($sql);
     <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
-<script>
-    function confirmDelete(id) {
-        var result = confirm("آیا میخواهید این موسیقی حذف شود؟");
-        if (result) {
-            // اگر کاربر تایید کرد، ارسال درخواست حذف به صفحه deletesongs.php
-            window.location.href = "deletesongs.php?id=" + id;
-        } else {
-            console.log("حذف رکورد لغو شد.");
-        }
-    }
-</script>
-
-<?php
-// بستن اتصال به پایگاه داده
-$conn->close();
-?>
-
-<!-- **************************************************** -->
+<!--footer -->
 <footer class="main-footer">
 <div class="float-right d-none d-sm-block">
  صفحه ادمین لنگوته
