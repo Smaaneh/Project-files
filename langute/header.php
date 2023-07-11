85420ae720dcf092bc63000899552f86e06e40ba
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-11 15:01:34 +0430

    add header.php & updatesongsfile

---
+++ langute/header.php
@@ -0,0 +1,59 @@
+<!-- Header -->
+<header class="header">
+	<!-- Header Inner -->
+	<div class="header-inner overlay">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-2 col-md-2 col-12">
+					<!-- Logo -->
+					<div class="logo">
+						<a href="index.html"><img src="images/logo.png" alt="#"></a>
+					</div>
+					<!--/ End Logo -->
+					<div class="mobile-menu"></div>
+				</div>
+				<div class="col-lg-8 col-md-8 col-12">
+					<div class="menu-bar">
+						<nav class="navbar navbar-default">
+							<div class="navbar-collapse">
+								<!-- Main Menu -->
+								<ul id="nav" class="nav menu navbar-nav">
+									<li><a href="contact.html"><i class="fa fa-address-book"></i>ارتباط با ما</a> </li>
+									<li><a href="events.html"><i class="fa fa-bullhorn"></i>مقالات</a></li>
+									<li><a href="#"><i class="fa fa-clone"></i>صفحه ها</a>
+										<ul class="dropdown">
+											<li><a href="teachers.html">استادان</a></li>
+											<li><a href="Social_learning.html">یادگیری اجتماعی</a></li>
+											<li><a href="Test.html">آزمون</a></li>
+											<li><a href="Progress_tracking.html">ردیابی پیشرفت</a></li>
+										</ul>
+									</li>
+									<li><a href="Learning.html"><i class="fa fa-clone"></i>یادگیری با روش های مختلف</a></li>
+									<li><a href="courses.html"><i class="fa fa-clone"></i>آموزش ها و درس ها</a></li>
+									<li class="active"><a href="index.html"><i class="fa fa-home"></i>خانه</a></li>
+								</ul>
+								<!-- End Main Menu -->
+							</div>
+						</nav>
+						<!-- login -->
+						<div class="user-icon">
+							<a href="login.php"><i class="fa fa-lg fa-user"></i></a>
+						</div>
+				<!-- Search Results -->
+				<div class="search-results">
+					<ul id="search-results"></ul>
+				</div>
+				<!-- Search Area -->
+				<div class="search-area">
+					<a href="#" class="icon"><i class="fa fa-search"></i></a>
+					<form class="search-form" action="search.php" method="post">
+						<input type="text" placeholder="جستجو ..." name="search" style="direction:rtl;">
+						<button value="search" type="submit"><i class="fa fa-search"></i></button>
+					</form>
+				</div>
+				<!-- End Search Area-->
+					</div>
+				</div>
+	<!--/ End Header Inner -->
+</header>
+<!--/ End Header -->
--- langute/songs.php
+++ langute/songs.php
@@ -17,12 +17,55 @@ $result = $conn->query($sql);
 <!DOCTYPE html>
 <html class="no-js" lang="fa">
 <head>
-    <title>پخش موسیقی</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- Meta Tags -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="keywords" content="Site keywords here">
+		<meta name="description" content="">
+		<meta name='copyright' content=''>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Title -->
+        <title>پخش موسیقی</title>
+        <!-- songs -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <!-- Favicon -->
+		<link rel="icon" type="image/png" href="images/favicon.png">
+
+        <!-- Web Font -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Font Awesome CSS -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <!-- Nice Select CSS -->
+        <link rel="stylesheet" href="css/niceselect.css">
+        <!-- Fancy Box CSS -->
+        <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+        <!-- Fancy Box CSS -->
+        <link rel="stylesheet" href="css/cube-portfolio.min.css">
+        <!-- Owl Carousel CSS -->
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <!-- Animate CSS -->
+        <link rel="stylesheet" href="css/animate.min.css">
+        <!-- Slick Nav CSS -->
+        <link rel="stylesheet" href="css/slicknav.min.css">
+        <!-- Magnific Popup -->
+        <link rel="stylesheet" href="css/magnific-popup.css">
+
+        <!-- Eduland Stylesheet -->
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+
+        <!-- Eduland Colors -->
+        <link rel="stylesheet" href="css/colors/color1.css">
+
 </head>
 <body>
+<?php include 'header.php';?>
     <div class="container">
         <?php while($row = $result->fetch_assoc()) { ?>
             <div class="card mb-3">
