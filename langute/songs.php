0e135443f540abeba8d0683a150830bfe804aa59
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-11 14:55:34 +0430

    Update songs.php

--- langute/songs.php
+++ langute/songs.php
@@ -10,3 +10,49 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("اتصال به دیتابیس با مشکل مواجه شد: " . $conn->connect_error);
 }
+// دریافت اطلاعات اهنگ‌ها از دیتابیس
+$sql = "SELECT * FROM songs";
+$result = $conn->query($sql);
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="fa">
+<head>
+    <title>پخش موسیقی</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <?php while($row = $result->fetch_assoc()) { ?>
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h3 class="card-title"><?php echo $row["title"]; ?></h3>
+                    <audio id="audio-<?php echo $row["id"]; ?>" controls>
+                        <source src="<?php echo $row["file_path"]; ?>" type="audio/mpeg">
+                        Your browser does not support the audio element.
+                    </audio>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+
+    <script>
+        function playAudio(id) {
+            var audio = document.getElementById("audio-" + id);
+            audio.play();
+        }
+
+        function pauseAudio(id) {
+            var audio = document.getElementById("audio-" + id);
+            audio.pause();
+        }
+
+        function changeSpeed(id) {
+            var audio = document.getElementById("audio-" + id);
+            var speedInput = document.getElementById("speed-" + id);
+            audio.playbackRate = speedInput.value;
+        }
+    </script>
+</body>
+</html>
