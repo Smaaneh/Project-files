187b0769468f0ceb6b583da5fd54dc1029664230
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-11 14:49:33 +0430

    connect DB

---
+++ langute/songs.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";  // نام سرور میزبان دیتابیس
+$username = "root";  // نام کاربری دیتابیس
+$password = "123";  // رمز عبور دیتابیس
+$dbname = "langute";  // نام دیتابیس
+
+// اتصال به دیتابیس
+$conn = new mysqli($servername, $username, $password, $dbname);
