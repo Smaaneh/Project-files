aaf8af41b2b8c6c5af6adfe0fd7c1892f1ddc261
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-11 20:45:50 +0430

    Create film.php

---
+++ langute/film.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+    <head>
+	<!-- metaTAGS -->
+	<?php include 'metaTAGS.php';?>
+		<!-- Title -->
+		<title>فیلم و کارتن</title>
+	<!-- linksCSS -->
+	<?php include 'linksCSS.php';?>
+    </head>
+    <body>
+      <!-- header-->
+<?php include 'header.php';?>
+
+		<!-- Breadcrumb -->
+		<div class="breadcrumbs overlay" style="background-image:url('images/breadcrumb-bg.jpg')">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-12">
+						<h2>فیلم های لنگوته</h2>
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<ul class="bread-list">
+							<li><a href="index.html"> خانه <i class="fa fa-angle-left"></i></a></li>
+              <li><a href="Learning.html">یادگیری با روش های مختلف<i class="fa fa-angle-left"></i></a></li>
+							<li class="active"><a href="#"> فیم و کارتن <i class="fa fa-angle-left"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/ End Breadcrumb -->
+
+		<!-- Courses -->
+		<section class="courses archive section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/Lesson number/lesson-1.jpg" alt="#">
+								<a href="grammer1.php" class="btn white primary">مشاهده فیلم ها </a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="grammer1.php"">فیلم های کمدی</a></h4>
+								<p>اگر به این ژانر فیلم ها علاقه دارید همین الان دیدن فیلم را شروع کنید </p>
+							</div>
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/Lesson number/lesson-2.jpg" alt="#">
+								<a href="grammer2.php" class="btn white primary">مشاهده انیمیشن ها</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="grammer2.php">انیمیشن های جذاب</a></h4>
+								<p>با دیدن انیمیشن های جذاب ، زبان خود را تقویت کنید</p>
+							</div>
+							<!--/ End Course Meta -->
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/Lesson number/lesson-3.jpg" alt="#">
+								<a href="grammer3.php" class="btn white primary">مشاهده فیلم ها</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="grammer3.php">فیلم های درام</a></h4>
+								<p>این ژانر فیلم رو از دست ندید ! حتمادر یادگیری زبان بهتون کمک میکنه</p>
+							</div>
+							<!--/ End Course Meta -->
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/Lesson number/lesson-4.jpg" alt="#">
+								<a href="grammer4.php" class="btn white primary">مشاهده فیلم ها</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="grammer4.php">فیلم های اکشن</a></h4>
+								<p>دیدن بهترین و معروف ترین فیلم های اکشن رو همین الان شروع کن!!</p>
+							</div>
+						</div>
+						<!--/ End Single Course -->
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ End Courses -->
+
+    <!-- Clients CSS -->
+    <div class="clients">
+      <div class="container">
+      </div>
+    </div>
+    <!--/ End Clients CSS -->
+
+    <!-- Footer -->
+    <footer class="footer section">
+      <!-- Footer Top -->
+      <div class="footer-top overlay">
+        <div class="container" style="text-align: right;">
+          <div class="row">
+            <div class="col-lg-4 col-md-6 col-12">
+              <!-- About -->
+              <div class="single-widget about">
+                <h2>راه های ارتباطی</h2>
+                <ul class="list">
+
+                  <li><i class="fa fa-phone"></i>تلفن : 09130000006 </li>
+                  <li><i class="fa fa-envelope"></i>ایمیل : <a href="mailto:info@youremail.com">langute@gmail.com</a></li>
+                </ul>
+                <!-- Social -->
+                <ul class="social">
+                  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                  <li class="active"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                  <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                  <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                  <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                </ul>
+                <!-- End Social -->
+              </div>
+              <!--/ End About -->
+            </div>
+            <div class="col-lg-4 col-md-6 col-12">
+              <!-- Useful Links -->
+              <div class="single-widget list">
+                <h2>لینک های مفید</h2>
+                <ul>
+                  <li><i class="fa fa-angle-left"></i><a href="index.html">خانه</a></li>
+                  <li><i class="fa fa-angle-left"></i><a href="contact.html">ارتباط با ما</a></li>
+                  <li><i class="fa fa-angle-left"></i><a href="courses.html">آموزش ها و درس ها</a></li>
+                  <li><i class="fa fa-angle-left"></i><a href="Learning_page.html">یادگیری با روش های مختف</a></li>
+                </ul>
+              </div>
+              <!--/ End Useful Links -->
+            </div>
+            <div class="col-lg-4 col-md-6 col-12">
+              <!-- Useful Links -->
+              <div class="single-widget opening-times">
+                <h2>ساعات پاسخ گویی</h2>
+                <ul class="list">
+                  <li><span>  08am - 10pm  </span><div class="value off">شنبه تا پنجشنبه</div></li>
+                  <li><span>  010am - 06pm  </span><div class="value off">جمعه و روزهای تعطیل</div></li>
+                </ul>
+              </div>
+              <!--/ End Useful Links -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+      <!--/ End Footer Top -->
+
+		<!-- Jquery JS-->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/jquery-migrate.min.js"></script>
+		<!-- Colors JS-->
+        <script src="js/colors.js"></script>
+		<!-- Popper JS-->
+        <script src="js/popper.min.js"></script>
+		<!-- Bootstrap JS-->
+        <script src="js/bootstrap.min.js"></script>
+		<!-- Owl Carousel JS-->
+        <script src="js/owl.carousel.min.js"></script>
+		<!-- Jquery Steller JS -->
+		<script src="js/jquery.stellar.min.js"></script>
+		<!-- Final Countdown JS -->
+		<script src="js/finalcountdown.min.js"></script>
+		<!-- Fancy Box JS-->
+		<script src="js/facnybox.min.js"></script>
+		<!-- Magnific Popup JS-->
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<!-- Circle Progress JS -->
+		<script src="js/circle-progress.min.js"></script>
+		<!-- Nice Select JS -->
+		<script src="js/niceselect.js"></script>
+		<!-- Jquery Steller JS-->
+        <script src="js/jquery.stellar.min.js"></script>
+		<!-- Jquery Steller JS-->
+        <script src="js/cube-portfolio.min.js"></script>
+		<!-- Slick Nav JS-->
+        <script src="js/slicknav.min.js"></script>
+		<!-- Easing JS-->
+        <script src="js/easing.min.js"></script>
+		<!-- Waypoints JS-->
+        <script src="js/waypoints.min.js"></script>
+		<!-- Counter Up JS -->
+		<script src="js/jquery.counterup.min.js"></script>
+		<!-- Scroll Up JS-->
+        <script src="js/jquery.scrollUp.min.js"></script>
+		<!-- Gmaps JS-->
+		<script src="js/gmaps.min.js"></script>
+		<!-- Main JS-->
+        <script src="js/main.js"></script>
+    </body>
+</html>
