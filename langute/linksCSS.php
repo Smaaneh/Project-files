10293d75e83e94bf2022249a0331fdbd801d3db7
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-11 16:55:26 +0430

    updte

---
+++ langute/linksCSS.php
@@ -0,0 +1,33 @@
+
+       <!-- Favicon -->
+		<link rel="icon" type="image/png" href="images/favicon.png">
+
+        <!-- Web Font -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Font Awesome CSS -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <!-- Nice Select CSS -->
+        <link rel="stylesheet" href="css/niceselect.css">
+        <!-- Fancy Box CSS -->
+        <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+        <!-- Fancy Box CSS -->
+        <link rel="stylesheet" href="css/cube-portfolio.min.css">
+        <!-- Owl Carousel CSS -->
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <!-- Animate CSS -->
+        <link rel="stylesheet" href="css/animate.min.css">
+        <!-- Slick Nav CSS -->
+        <link rel="stylesheet" href="css/slicknav.min.css">
+        <!-- Magnific Popup -->
+        <link rel="stylesheet" href="css/magnific-popup.css">
+
+        <!-- Eduland Stylesheet -->
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+
+        <!-- Eduland Colors -->
+        <link rel="stylesheet" href="css/colors/color1.css">
---
+++ langute/metaTAGS.php
@@ -0,0 +1,7 @@
+           <!-- Meta Tags -->
+           <meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="keywords" content="Site keywords here">
+		<meta name="description" content="">
+		<meta name='copyright' content=''>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
