5f2fd62388cd23c13b4506d144fa50319bdebf04
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-12 14:53:55 +0430

    add music

---
+++ langute/Music.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html class="no-js" lang="fa">
+    <head>
+	<!-- metaTAGS -->
+	<?php include 'metaTAGS.php';?>
+		<!-- Title -->
+		<title>موزیک و پادکست</title>
+	<!-- linksCSS -->
+	<?php include 'linksCSS.php';?>
+    </head>
+    <body>
+      <!-- header-->
+	<?php include 'header.php';?>
+
+		<!-- Breadcrumb -->
+		<div class="breadcrumbs overlay" style="background-image:url('images/breadcrumb-bg.jpg')">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-12">
+						<h2>موزیک و پادکست</h2>
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<ul class="bread-list">
+							<li><a href="index.html"> خانه <i class="fa fa-angle-left"></i></a></li>
+                            <li><a href="Learning.html">یادگیری با روش های مختلف<i class="fa fa-angle-left"></i></a></li>
+							<li class="active"><a href="#">پادکست و موزیک<i class="fa fa-angle-left"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/ End Breadcrumb -->
+
+		<!-- Courses -->
+		<section class="courses archive section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images\films/comedy.jpg" alt="#">
+								<a href="comedy.php" class="btn white primary">دیدن صفحه</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="comedy.php"">موزیک های شاد</a></h4>
+								<p>با گوش دادن به موزیک های شاد زبان خود را تقویت کنید</p>
+							</div>
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/Lesson number/lesson-2.jpg" alt="#">
+								<a href="Animation.php" class="btn white primary">دیدن صفحه</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="Animation.php">پادکست های انگیزشی</a></h4>
+								<p>پادکست هایی به زبان انگلیسی </p>
+							</div>
+							<!--/ End Course Meta -->
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/Lesson number/lesson-3.jpg" alt="#">
+								<a href="drama.php" class="btn white primary">دیدن صفحه</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="drama.php">موسیقی های آرام و غمگین</a></h4>
+								<p>با گوش دادن به این نوع موسیقی پیشرفتتانم در زبان آموزی را دوچندان کنید</p>
+							</div>
+							<!--/ End Course Meta -->
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/Lesson number/lesson-4.jpg" alt="#">
+								<a href="action.php" class="btn white primary">دیدن صفحه</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="action.php">پادکست های علمی</a></h4>
+								<p>علم و زبان خود را با گوش دادن به این کوسیقی زیاد کنید</p>
+							</div>
+						</div>
+						<!--/ End Single Course -->
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ End Courses -->
+
+   <!-- foother & js links -->
+   <?php include 'foother.php';?>
+    </body>
+</html>
