8a86547440d36b21b02c8620f152a58ce4bc3d57
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-13 13:16:53 +0430

    Converting courses to PHP and including

---
+++ langute/courses.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html class="no-js" lang="fa">
+    <head>
+		<!-- metaTAGS -->
+		<?php include 'metaTAGS.php';?>
+
+		<!-- Title -->
+		<title>آموزش ها و درس ها</title>
+
+		<!-- linksCSS -->
+		<?php include 'linksCSS.php';?>
+
+    </head>
+    <body>
+
+       <!-- header-->
+	   <?php include 'header.php';?>
+
+
+		<!-- Breadcrumb -->
+		<div class="breadcrumbs overlay" style="background-image:url('images/breadcrumb-bg.jpg')">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-12">
+						<h2>آموزش ها و درس ها</h2>
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<ul class="bread-list">
+							<li><a href="index.php">خانه <i class="fa fa-angle-left"></i></a></li>
+							<li class="active"><a href="courses.php"> آموزش ها و درس ها <i class="fa fa-angle-left"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/ End Breadcrumb -->
+
+		<!-- Courses -->
+		<section class="courses archive section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/courses/course1.jpg" alt="#">
+								<a href="course-single.html" class="btn white primary">شروع آموزش</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<div class="name-price">
+									<div class="teacher-info">
+										<img src="images/author1.jpg" alt="#">
+										<h4 class="title">الناز محمدی</h4>
+									</div>
+									<span class="price">$350</span>
+								</div>
+								<h4 class="c-title"><a href="speaking.html">(speaking)گفتار</a></h4>
+								<p>با شروع این بخش شما در مهارت گفتار قوی خواهید شد</p>
+							</div>
+							<!-- Course Meta -->
+							<div class="course-meta">
+								<!-- Rattings -->
+								<ul style="text-align:right;" class="rattings">
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star-half-o"></i></li>
+									<li class="point"><span>4.5</span></li>
+								</ul>
+								<!-- Course Info -->
+								<div class="course-info">
+                  <span><i class="fa fa-users"></i>2.4k دانشجویان</span>
+                  <span><i class="fa fa-calendar-o"></i>2 ماه</span>
+									<span><i class="fa fa-clock-o"></i>09:30 - 12:00</span>
+								</div>
+							</div>
+							<!--/ End Course Meta -->
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/courses/course3.jpg" alt="#">
+								<a href="words.html" class="btn white primary">شروع آموزش</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<div class="name-price">
+									<div class="teacher-info">
+										<img src="images/author3.jpg" alt="#">
+										<h4 class="title">نرگس افراز</h4>
+									</div>
+									<span class="price">رایگان</span>
+								</div>
+								<h4 class="c-title"><a href="words.php">(vocabulary)واژگان</a></h4>
+								<p>عبارات ، جملات  و واژگان زبان انگلیسی</p>
+							</div>
+							<!-- Course Meta -->
+							<div class="course-meta">
+								<!-- Rattings -->
+								<ul style="text-align:right;" class="rattings">
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star-o"></i></li>
+									<li class="point"><span>4.0</span></li>
+								</ul>
+								<!-- Course Info -->
+								<div class="course-info">
+									<span><i class="fa fa-users"></i>5.3kدانشجویان</span>
+									<span><i class="fa fa-calendar-o"></i>2هفته</span>
+									<span><i class="fa fa-clock-o"></i>10:00 - 11:00</span>
+								</div>
+							</div>
+							<!--/ End Course Meta -->
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/courses/course2.jpg" alt="#">
+								<a href="grammers.html" class="btn white primary">شروع آموزش</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<div class="name-price">
+									<div class="teacher-info">
+										<img src="images/author2.jpg" alt="#">
+										<h4 class="title">مهیار فلاح</h4>
+									</div>
+									<span class="price">$290</span>
+								</div>
+								<h4 class="c-title"><a href="grammers.html">(grammar)گرامر و قواید دستور زبان</a></h4>
+								<p>آموزش گرامر زبان انگلیسی از صفر تا صد</p>
+							</div>
+							<!-- Course Meta -->
+							<div class="course-meta">
+								<!-- Rattings -->
+								<ul style="text-align:right;" class="rattings">
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star-half-o"></i></li>
+									<li><i class="fa fa-star-o"></i></li>
+									<li class="point"><span>3.9</span></li>
+								</ul>
+								<!-- Course Info -->
+								<div class="course-info">
+									<span><i class="fa fa-users"></i>2.4k دانشجویان</span>
+									<span><i class="fa fa-calendar-o"></i> 2ماه </span>
+									<span><i class="fa fa-clock-o"></i>10:30 - 12:30</span>
+								</div>
+							</div>
+							<!--/ End Course Meta -->
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/courses/course4.jpg" alt="#">
+								<a href="conversation.html" class="btn white primary">شروع آموزش</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<div class="name-price">
+									<div class="teacher-info">
+										<img src="images/author1.jpg" alt="#">
+										<h4 class="title">الناز محمدی</h4>
+									</div>
+									<span class="price">$550</span>
+								</div>
+								<h4 class="c-title"><a href="conversation.html">(conversion)مکالمه</a></h4>
+								<p>آموزش مکالمه زبان انگلیسی از صفر تا صد</p>
+							</div>
+							<!-- Course Meta -->
+							<div class="course-meta">
+								<!-- Rattings -->
+								<ul  style="text-align:right;" class="rattings">
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star"></i></li>
+									<li><i class="fa fa-star-half-o"></i></li>
+									<li class="point"><span>4.9</span></li>
+								</ul>
+								<!-- Course Info -->
+								<div class="course-info">
+									<span><i class="fa fa-users"></i>2.4k دانشجویان</span>
+									<span><i class="fa fa-calendar-o"></i>2ماه</span>
+									<span><i class="fa fa-clock-o"></i>12:30 - 14:30</span>
+								</div>
+							</div>
+							<!--/ End Course Meta -->
+						</div>
+						<!--/ End Single Course -->
+					</div>
+
+				</div>
+			</div>
+		</section>
+		<!--/ End Courses -->
+
+   <!-- foother & js links -->
+   <?php include 'foother.php';?>
+    </body>
+</html>
