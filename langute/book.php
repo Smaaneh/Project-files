b81a0975fad53eb184a51869a7edb39a2585f6ac
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-13 13:38:50 +0430

    Update book.php

---
+++ langute/book.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html class="no-js" lang="fa">
+    <head>
+	<!-- metaTAGS -->
+	<?php include 'metaTAGS.php';?>
+		<!-- Title -->
+		<title>کتاب و داستان</title>
+	<!-- linksCSS -->
+	<?php include 'linksCSS.php';?>
+    </head>
+    <body>
+      <!-- header-->
+	<?php include 'header.php';?>
+
+		<!-- Breadcrumb -->
+		<div class="breadcrumbs overlay" style="background-image:url('images/breadcrumb-bg.jpg')">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-12">
+						<h2>کتاب و داستان</h2>
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<ul class="bread-list">
+							<li><a href="index.php"> خانه <i class="fa fa-angle-left"></i></a></li>
+              <li><a href="Learning.php">یادگیری با روش های مختلف<i class="fa fa-angle-left"></i></a></li>
+							<li class="active"><a href="#"> کتاب و داستان <i class="fa fa-angle-left"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/ End Breadcrumb -->
+
+		<!-- Courses -->
+		<section class="courses archive section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images\films/comedy.jpg" alt="#">
+								<a href="Romance.php" class="btn white primary">مشاهده و خواندن</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="Romance.php"">فیلم های کمدی</a></h4>
+								<p>اگر به این ژانر فیلم ها علاقه دارید همین الان دیدن فیلم را شروع کنید </p>
+							</div>
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/Lesson number/lesson-2.jpg" alt="#">
+								<a href="Animation.php" class="btn white primary">مشاهده و خواندن</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="Animation.php">انیمیشن های جذاب</a></h4>
+								<p>با دیدن انیمیشن های جذاب ، زبان خود را تقویت کنید</p>
+							</div>
+							<!--/ End Course Meta -->
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/Lesson number/lesson-3.jpg" alt="#">
+								<a href="drama.php" class="btn white primary">مشاهده و خواندن</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="drama.php">فیلم های درام</a></h4>
+								<p>این ژانر فیلم رو از دست ندید ! حتمادر یادگیری زبان بهتون کمک میکنه</p>
+							</div>
+							<!--/ End Course Meta -->
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/Lesson number/lesson-4.jpg" alt="#">
+								<a href="action.php" class="btn white primary">مشاهده و خواندن</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="action.php">فیلم های اکشن</a></h4>
+								<p>دیدن بهترین و معروف ترین فیلم های اکشن رو همین الان شروع کن!!</p>
+							</div>
+						</div>
+						<!--/ End Single Course -->
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ End Courses -->
+
+   <!-- foother & js links -->
+   <?php include 'foother.php';?>
+    </body>
+</html>
