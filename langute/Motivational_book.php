56e2e7cba965a5616c1388f0f77e8466f8cc87ee
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-13 21:45:51 +0430

    Update Motivational_book.php

---
+++ langute/Motivational_book.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";  // نام سرور میزبان دیتابیس
+$username = "root";  // نام کاربری دیتابیس
+$password = "123";  // رمز عبور دیتابیس
+$dbname = "langute";  // نام دیتابیس
+
+// اتصال به دیتابیس
+$conn = new mysqli($servername, $username, $password, $dbname);
+// بررسی اتصال
+if ($conn->connect_error) {
+    die("اتصال به دیتابیس با مشکل مواجه شد: " . $conn->connect_error);
+}
