4ccb5b99c2c48e7db9686dc35ef87ba034459e13
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-13 14:11:52 +0430

    Update Book.php

--- langute/AdminPanel/pages/learning/book/Book.php
+++ langute/AdminPanel/pages/learning/book/Book.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>مدیریت | فیلم و کارتن</title>
+  <title>مدیریت | کتاب و داستان</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
