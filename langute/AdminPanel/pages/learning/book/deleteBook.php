05b33aa0b244eb6f7c4254f01a796e16321ec98a
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-13 14:09:22 +0430

    Adding book management files

---
+++ langute/AdminPanel/pages/learning/book/deleteBook.php
@@ -0,0 +1,40 @@
+<?php
+// اطلاعات اتصال به پایگاه داده
+$servername = "localhost"; // آدرس سرور پایگاه داده
+$username = "root"; // نام کاربری پایگاه داده
+$password = "123"; // رمز عبور پایگاه داده
+$dbname = "langute"; // نام پایگاه داده
+
+// ایجاد اتصال به پایگاه داده
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// بررسی وضعیت اتصال
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// دریافت شناسه کلمه برای حذف
+$id = $_GET['id'];
+
+// دریافت عنوان قبل از حذف
+$sql = "SELECT Title FROM movie WHERE id = $id";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $Title = $row['Title'];
+
+    // حذف رکورد مربوطه از جدول movie
+    $sql = "DELETE FROM movie WHERE id = $id";
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>alert('فیلم $Title حذف شد.')</script>";
+        echo "<script>window.location.href = 'movie.php';</script>";
+        
+    } else {
+        echo "<script>alert('خطا در حذف رکورد.')</script>";
+    }
+} else {
+    echo "<script>alert('رکورد مورد نظر یافت نشد.')</script>";
+}
+
+// بستن اتصال به پایگاه داده
+$conn->close();
+?>
