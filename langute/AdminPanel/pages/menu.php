7cefacbb87e7d85412d4d64dab6c389d1dca6109
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-14 22:22:41 +0430

    update include

--- langute/AdminPanel/pages/menu.php
+++ langute/AdminPanel/pages/menu.php
@@ -1,7 +1,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="../ HomeAdmin.php" class="brand-link">
+    <a href="../HomeAdmin.html" class="brand-link">
       <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">پنل مدیریت</span>
