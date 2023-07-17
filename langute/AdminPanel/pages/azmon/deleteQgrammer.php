2a52b723a262386aa48290c9974281601304d58b
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-17 20:14:20 +0430

    Update deleteQgrammer.php

---
+++ langute/AdminPanel/pages/azmon/deleteQgrammer.php
@@ -0,0 +1,42 @@
+<?php
+// اطلاعات اتصال به پایگاه داده
+$servername = "localhost"; // آدرس سرور پایگاه داده
+$username = "root"; // نام کاربری پایگاه داده
+$password = "123"; // رمز عبور پایگاه داده
+$dbname = "langute"; // نام پایگاه داده
+
+// ایجاد اتصال به پایگاه داده
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// بررسی وضعیت اتصال
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+if (isset($_GET['question_id'])) {
+    // دریافت شناسه سوال برای حذف
+    $id = $_GET['question_id'];
+  // دریافت عنوان قبل از حذف
+  $sql = "SELECT question_title FROM questions WHERE question_id = $id";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $question_title = $row['question_title'];
+
+    // حذف رکورد مربوطه از جدول questions
+    $sql = "DELETE FROM questions WHERE question_id = $question_id";
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>alert('سوال $question_title حذف شد.')</script>";
+        echo "<script>window.location.href = 'Qgrammer.php';</script>";
+        
+    } else {
+        echo "<script>alert('خطا در حذف رکورد.')</script>";
+    }
+} else {
+    echo "<script>alert('رکورد مورد نظر یافت نشد.')</script>";
+}
+} else {
+    echo "شناسه سوال معتبر نیست.";
+}
+// بستن اتصال به پایگاه داده
+$conn->close();
+?>
