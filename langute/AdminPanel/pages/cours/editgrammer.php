0004030a9581a2fb21c6c8067a0a2245d97cc56c
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-18 18:51:53 +0430

    Update editgrammer.php

--- langute/AdminPanel/pages/cours/editgrammer.php
+++ langute/AdminPanel/pages/cours/editgrammer.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>مدیریت | واژگان</title>
+  <title>مدیریت | گرامر</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
