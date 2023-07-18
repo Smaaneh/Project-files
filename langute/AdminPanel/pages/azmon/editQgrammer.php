3a11bd237df409e52c7a4acbf7f43d3d5a8c9d42
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-18 16:22:40 +0430

    Update editQgrammer.php

--- langute/AdminPanel/pages/azmon/editQgrammer.php
+++ langute/AdminPanel/pages/azmon/editQgrammer.php
@@ -34,7 +34,24 @@
           </div>
         </div>
       </section>
+ <!--php code -->
+ <?php
+        // اطلاعات اتصال به پایگاه داده
+        $servername = "localhost"; // آدرس سرور پایگاه داده
+        $username = "root"; // نام کاربری پایگاه داده
+        $password = "123"; // رمز عبور پایگاه داده
+        $dbname = "langute"; // نام پایگاه داده
 
+        // ایجاد اتصال به پایگاه داده
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // بررسی وضعیت اتصال
+        if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+        }
+            // بستن اتصال به پایگاه داده
+    $conn->close();
+    ?>
     <!-- footer -->
     <?php include '../footer.php';?>
   <!-- JSlinks -->
