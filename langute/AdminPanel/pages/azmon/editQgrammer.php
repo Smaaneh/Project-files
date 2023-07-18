2271920ba0ba0c647d8e90edcc07541dbfbfae86
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-18 16:19:41 +0430

    Update editQgrammer.php

---
+++ langute/AdminPanel/pages/azmon/editQgrammer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>مدیریت | آزمون گرامر</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- CSSlinks -->
+  <?php include '../CSSlinks.php';?>
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+  <?php include '../navbar.php';?>
+
+  <?php include '../menu.php';?>
+
+
+    <!-- footer -->
+    <?php include '../footer.php';?>
+  <!-- JSlinks -->
+  <?php include '../JSlinks.php';?>
+</body>
+</html>
