8688925b43b4cabc56567d2f3937492fcd6bbe13
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-26 16:37:51 +0430

    Update phpmailer.lang-fa.php

--- langute/PHPMailer/language/phpmailer.lang-fa.php
+++ langute/PHPMailer/language/phpmailer.lang-fa.php
@@ -1,26 +1,28 @@
 <?php
-/**
-* PHPMailer language file: refer to English translation for definitive list
-* Persian (Farsi) Version, UTF-8
-* by : Vahid Mirzaei <info@webgoo.ir>
-*/
 
-$PHPMAILER_LANG['authenticate']         = 'SMTP Error: تطبیق اطلاعات کاربری جهت اتصال، انجام نشد.';
-$PHPMAILER_LANG['connect_host']         = 'SMTP Error: ارتباط با میزبان SMTP میسر نیست.';
-$PHPMAILER_LANG['data_not_accepted']    = 'SMTP Error: اطلاعات ارسالی از طرف سرور پذیرفته نشد.';
-$PHPMAILER_LANG['empty_message']        = 'متن پیام خالی است.';
-$PHPMAILER_LANG['encoding']             = 'یونیکد قابل استفاده نیست: ';
-$PHPMAILER_LANG['execute']              = 'برنامه قابل اجرا شدن نیست: ';
-$PHPMAILER_LANG['file_access']          = 'دسترسی به فایل امکان پذیر نیست: ';
-$PHPMAILER_LANG['file_open']            = 'فایل قابل باز شدن نیست: ';
-$PHPMAILER_LANG['from_failed']          = 'آدرس ارسالی زیر صحیح نیست: ';
-$PHPMAILER_LANG['instantiate']          = 'نمونه سازی تابع ارسال ایمیل انجام نشد.';
-$PHPMAILER_LANG['invalid_email']        = 'ایمیل ارسال نشد، آدرس زیر را بررسی کنید: ';
-$PHPMAILER_LANG['mailer_not_supported'] = 'برنامه پشتیبانی نمی شود.';
-$PHPMAILER_LANG['provide_address']      = 'حداقل یک دریافت کننده تعیین کنید.';
-$PHPMAILER_LANG['recipients_failed']    = 'SMTP Error: دریافت کنندگان زیر را بررسی کنید: ';
-$PHPMAILER_LANG['signing']              = 'خطا در ورود به سیستم: ';
-$PHPMAILER_LANG['smtp_connect_failed']  = 'خطا در اتصال به سرور SMTP.';
-$PHPMAILER_LANG['smtp_error']           = 'اتصال به سرور SMTP میسر نیست: ';
-$PHPMAILER_LANG['variable_set']         = 'تنظیم یا منقضی کردن متغیرها با مشکل مواجه شد: ';
-?>
+/**
+ * Persian/Farsi PHPMailer language file: refer to English translation for definitive list
+ * @package PHPMailer
+ * @author Ali Jazayeri <jaza.ali@gmail.com>
+ * @author Mohammad Hossein Mojtahedi <mhm5000@gmail.com>
+ */
+
+$PHPMAILER_LANG['authenticate']         = 'خطای SMTP: احراز هویت با شکست مواجه شد.';
+$PHPMAILER_LANG['connect_host']         = 'خطای SMTP: اتصال به سرور SMTP برقرار نشد.';
+$PHPMAILER_LANG['data_not_accepted']    = 'خطای SMTP: داده‌ها نا‌درست هستند.';
+$PHPMAILER_LANG['empty_message']        = 'بخش متن پیام خالی است.';
+$PHPMAILER_LANG['encoding']             = 'کد‌گذاری نا‌شناخته: ';
+$PHPMAILER_LANG['execute']              = 'امکان اجرا وجود ندارد: ';
+$PHPMAILER_LANG['file_access']          = 'امکان دسترسی به فایل وجود ندارد: ';
+$PHPMAILER_LANG['file_open']            = 'خطای File: امکان بازکردن فایل وجود ندارد: ';
+$PHPMAILER_LANG['from_failed']          = 'آدرس فرستنده اشتباه است: ';
+$PHPMAILER_LANG['instantiate']          = 'امکان معرفی تابع ایمیل وجود ندارد.';
+$PHPMAILER_LANG['invalid_address']      = 'آدرس ایمیل معتبر نیست: ';
+$PHPMAILER_LANG['mailer_not_supported'] = ' mailer پشتیبانی نمی‌شود.';
+$PHPMAILER_LANG['provide_address']      = 'باید حداقل یک آدرس گیرنده وارد کنید.';
+$PHPMAILER_LANG['recipients_failed']    = 'خطای SMTP: ارسال به آدرس گیرنده با خطا مواجه شد: ';
+$PHPMAILER_LANG['signing']              = 'خطا در امضا: ';
+$PHPMAILER_LANG['smtp_connect_failed']  = 'خطا در اتصال به SMTP.';
+$PHPMAILER_LANG['smtp_error']           = 'خطا در SMTP Server: ';
+$PHPMAILER_LANG['variable_set']         = 'امکان ارسال یا ارسال مجدد متغیر‌ها وجود ندارد: ';
+$PHPMAILER_LANG['extension_missing']    = 'افزونه موجود نیست: ';
