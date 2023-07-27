cb3f3ecd9aa8f1413746ad6bf973609a4d8153c7
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-27 15:47:33 +0430

    Update addarticles.php

---
+++ langute/AdminPanel/pages/articles/addarticles.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>پنل مدیریت | افزودن مقالات</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
