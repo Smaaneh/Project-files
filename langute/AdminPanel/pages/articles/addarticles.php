58bf164f95feb362dc3b6197d29956d67f2298b5
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-27 16:03:03 +0430

    Update addarticles.php

--- langute/AdminPanel/pages/articles/addarticles.php
+++ langute/AdminPanel/pages/articles/addarticles.php
@@ -3,6 +3,113 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>پنل مدیریت | افزودن مقالات</title>
+  <title>پنل مدیریت | ویرایشگر</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- CSSlinks -->
+ <?php include '../CSSlinks.php';?>
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="../../plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+ <!-- Navbar -->
+ <?php include '../navbar.php';?>
+<!-- menu -->
+  <?php include '../menu.php';?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>ویرایشگر متن</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-left">
+              <li class="breadcrumb-item"><a href="#">خانه</a></li>
+              <li class="breadcrumb-item active">ویرایشگر متن</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card card-info card-outline">
+            <div class="card-header">
+              <h3 class="card-title">
+                CKEditor5
+                <small>پیشرفته به همراه همه امکانات</small>
+              </h3>
+              <!-- tools box -->
+              <div class="card-tools">
+                <button type="button" class="btn btn-tool btn-sm"
+                        data-widget="collapse"
+                        data-toggle="tooltip"
+                        title="Collapse">
+                  <i class="fa fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool btn-sm"
+                        data-widget="remove"
+                        data-toggle="tooltip"
+                        title="Remove">
+                  <i class="fa fa-times"></i>
+                </button>
+              </div>
+              <!-- /. tools -->
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <div class="mb-3">
+                <textarea id="editor1" name="editor1" style="width: 100%">لطفا متن مورد نظر خودتان را وارد کنید</textarea>
+              </div>
+              <p class="text-sm mb-0">مشاهده مستندات مربوط به این ویرایشگر متن <a href="https://ckeditor.com/ckeditor-5-builds/#classic">CKEditor</a>
+              </p>
+            </div>
+          </div>
+          <!-- /.card -->
+        </div>
+        <!-- /.col-->
+      </div>
+      <!-- ./row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <!-- footer -->
+<?php include '../footer.php';?>
+<!-- JSlinks -->
+<?php include '../JSlinks.php';?>
+
+<!-- CK Editor -->
+<script src="../../plugins/ckeditor/ckeditor.js"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="../../plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<script>
+  $(function () {
+    // Replace the <textarea id="editor1"> with a CKEditor
+    // instance, using default configuration.
+    ClassicEditor
+      .create(document.querySelector('#editor1'))
+      .then(function (editor) {
+        // The editor instance
+      })
+      .catch(function (error) {
+        console.error(error)
+      })
+
+    // bootstrap WYSIHTML5 - text editor
+
+      $('.textarea').wysihtml5({
+          toolbar: { fa: true }
+      })
+  })
+</script>
+</body>
+</html>
