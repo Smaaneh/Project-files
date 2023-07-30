8bd56522aae0f2e46a912e35fe4060d15087a6cf
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-07-31 00:15:51 +0430

    Update addQgrammer.php

--- langute/AdminPanel/pages/azmon/addQgrammer.php
+++ langute/AdminPanel/pages/azmon/addQgrammer.php
@@ -35,108 +35,106 @@
           </div>
         </div>
       </section>
-       <!--php code -->
-        <?php
-        // اطلاعات اتصال به پایگاه داده
-        $servername = "localhost"; // آدرس سرور پایگاه داده
-        $username = "root"; // نام کاربری پایگاه داده
-        $password = "123"; // رمز عبور پایگاه داده
-        $dbname = "langute"; // نام پایگاه داده
+      <?php
+// اطلاعات اتصال به پایگاه داده
+$servername = "localhost"; // آدرس سرور پایگاه داده
+$username = "root"; // نام کاربری پایگاه داده
+$password = "123"; // رمز عبور پایگاه داده
+$dbname = "langute"; // نام پایگاه داده
 
-        // ایجاد اتصال به پایگاه داده
-        $conn = new mysqli($servername, $username, $password, $dbname);
+// ایجاد اتصال به پایگاه داده
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-        // بررسی وضعیت اتصال
-        if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-        }
+// بررسی وضعیت اتصال
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-        // پیام‌های خالی برای پیام‌های نمایشی به کاربر
-        $successMessage = $errorMessage = "";
+// پیام‌های خالی برای پیام‌های نمایشی به کاربر
+$successMessage = $errorMessage = "";
 
-        // درخواست ارسال فرم
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-          // دریافت مقادیر فرم
-          $questionTitle = $_POST["question_title"];
-          $correctOption = $_POST["correct_option"];
-          $wrongOption1 = $_POST["wrong_option_1"];
-          $wrongOption2 = $_POST["wrong_option_2"];
-          $wrongOption3 = $_POST["wrong_option_3"];
+// درخواست ارسال فرم
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // دریافت مقادیر فرم
+    $questionTitle = $_POST["question_title"];
+    $correctOption = $_POST["correct_option"];
+    $wrongOption1 = $_POST["wrong_option_1"];
+    $wrongOption2 = $_POST["wrong_option_2"];
+    $wrongOption3 = $_POST["wrong_option_3"];
+
     // استفاده از تراکنش‌ها برای انجام دو عملیات INSERT به صورت اتمیک
     $conn->autocommit(false);
-        // استخراج اطلاعات سوالات گرامری به دیتابیس
-        $sql = "INSERT INTO questions (category, question_title, correct_option) VALUES ('grammer', '$questionTitle', '$correctOption')";
+
+    // استخراج اطلاعات سوالات گرامری به دیتابیس
+    $sql = "INSERT INTO questions (category, question_title) VALUES ('grammer', '$questionTitle')";
+    if ($conn->query($sql) === TRUE) {
+        $questionId = $conn->insert_id;
+
+        // استخراج گزینه‌ها به دیتابیس
+        $sql = "INSERT INTO options (question_id, option_text, correct_option) VALUES ";
+        $sql .= "($questionId, '$correctOption', 1), ";
+        $sql .= "($questionId, '$wrongOption1', 0), ";
+        $sql .= "($questionId, '$wrongOption2', 0), ";
+        $sql .= "($questionId, '$wrongOption3', 0)";
+
         if ($conn->query($sql) === TRUE) {
-            $questionId = $conn->insert_id;
-
-            // استخراج گزینه‌ها به دیتابیس
-            $sql = "INSERT INTO options (question_id, option_text) VALUES ";
-            $sql .= "($questionId, '$correctOption'), ";
-            $sql .= "($questionId, '$wrongOption1'), ";
-            $sql .= "($questionId, '$wrongOption2'), ";
-            $sql .= "($questionId, '$wrongOption3')";
-
-            if ($conn->query($sql) === TRUE) {
-                // اعمال تراکنش‌ها
-                $conn->commit();
-                $successMessage = "سوال با موفقیت ثبت شد.";
-            } else {
-                // انصراف از تراکنش و برگرداندن تغییرات
-                $conn->rollback();
-                $errorMessage = "خطا در ثبت گزینه‌ها: " . $conn->error;
-            }
+            // اعمال تراکنش‌ها
+            $conn->commit();
+            $successMessage = "سوال با موفقیت ثبت شد.";
         } else {
             // انصراف از تراکنش و برگرداندن تغییرات
             $conn->rollback();
-            $errorMessage = "خطا در ثبت سوال: " . $conn->error;
+            $errorMessage = "خطا در ثبت گزینه‌ها: " . $conn->error;
         }
-
-        // فعال‌سازی خودکار COMMIT برای تراکنش‌های بعدی
-        $conn->autocommit(true);
+    } else {
+        // انصراف از تراکنش و برگرداندن تغییرات
+        $conn->rollback();
+        $errorMessage = "خطا در ثبت سوال: " . $conn->error;
     }
 
-    // بستن اتصال به پایگاه داده
-    $conn->close();
-    ?>
+    // فعال‌سازی خودکار COMMIT برای تراکنش‌های بعدی
+    $conn->autocommit(true);
+}
 
-      <div class="container">
-        <div class="col-lg-6 offset-lg-3 col-12">
-          <h2>افزودن سوال گرامری</h2>
-          <?php if (!empty($successMessage)): ?>
+// بستن اتصال به پایگاه داده
+$conn->close();
+?>
+
+<div class="container">
+    <div class="col-lg-6 offset-lg-3 col-12">
+        <h2>افزودن سوال گرامری</h2>
+        <?php if (!empty($successMessage)): ?>
             <div class="alert alert-success"><?php echo $successMessage; ?></div>
-          <?php endif; ?>
-          <?php if (!empty($errorMessage)): ?>
+        <?php endif; ?>
+        <?php if (!empty($errorMessage)): ?>
             <div class="alert alert-danger"><?php echo $errorMessage; ?></div>
-          <?php endif; ?>
-          <form action="addQgrammer.php" method="post">
+        <?php endif; ?>
+        <form action="addQgrammer.php" method="post">
             <div class="form-group">
-              <label>عنوان سوال</label>
-              <input type="text" class="form-control" name="question_title" placeholder="وارد کردن اطلاعات ..." required>
+                <label>عنوان سوال</label>
+                <input type="text" class="form-control" name="question_title" placeholder="وارد کردن اطلاعات ..." required>
             </div>
             <div class="form-group has-success">
-              <label class="control-label text-success" for="inputSuccess" for="inputSuccess"><i class="fa fa-check"></i>گزینه درست</label>
-              <input type="text" class="form-control" name="correct_option" id="correct_option" placeholder="وارد کردن اطلاعات ..." required>
+                <label class="control-label text-success" for="inputSuccess"><i class="fa fa-check"></i>گزینه درست</label>
+                <input type="text" class="form-control" name="correct_option" id="correct_option" placeholder="وارد کردن اطلاعات ..." required>
             </div>
             <div class="form-group has-error">
-              <label class="control-label text-danger" for="inputError"><i class="fa fa-times-circle-o"></i>گزینه غلط ۱</label>
-              <input type="text" class="form-control" name="wrong_option_1" id="wrong_option_1" placeholder="وارد کردن اطلاعات ..." required>
+                <label class="control-label text-danger" for="inputError"><i class="fa fa-times-circle-o"></i>گزینه غلط ۱</label>
+                <input type="text" class="form-control" name="wrong_option_1" id="wrong_option_1" placeholder="وارد کردن اطلاعات ..." required>
             </div>
-
             <div class="form-group has-error">
-              <label class="control-label text-danger" for="inputError"><i class="fa fa-times-circle-o"></i>گزینه غلط ۲</label>
-              <input type="text" class="form-control" name="wrong_option_2" id="wrong_option_2" placeholder="وارد کردن اطلاعات ..." required>
+                <label class="control-label text-danger" for="inputError"><i class="fa fa-times-circle-o"></i>گزینه غلط ۲</label>
+                <input type="text" class="form-control" name="wrong_option_2" id="wrong_option_2" placeholder="وارد کردن اطلاعات ..." required>
             </div>
-
             <div class="form-group has-error">
-              <label class="control-label text-danger" for="inputError"><i class="fa fa-times-circle-o"></i>گزینه غلط ۳</label>
-              <input type="text" class="form-control" name="wrong_option_3" placeholder="وارد کردن اطلاعات ..." required>
+                <label class="control-label text-danger" for="inputError"><i class="fa fa-times-circle-o"></i>گزینه غلط ۳</label>
+                <input type="text" class="form-control" name="wrong_option_3" placeholder="وارد کردن اطلاعات ..." required>
             </div>
 
             <button type="submit" class="btn btn-block btn-success btn-sm">افزودن</button>
-          </form>
-        </div>
-      </div>
+        </form>
     </div>
+</div>
   <!-- footer -->
   <?php include '../footer.php';?>
   <!-- JSlinks -->
