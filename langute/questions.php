7e8d90287b9996d3c66571c65d6ca3fe2e62a443
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-08-01 19:29:13 +0430

    Display a question on the page

--- langute/questions.php
+++ langute/questions.php
@@ -51,38 +51,55 @@
 
         if ($category) {
             $questions = get_questions_by_category($conn, $category);
-            if ($questions->num_rows > 0) {
-                while ($row = $questions->fetch_assoc()) {
-                    $question_id = $row['question_id'];
-                    $question_title = $row['question_title'];
-                    echo '<div class="question ml-sm-5 pl-sm-5 pt-2">';
-                    echo '<div class="py-2 h5"><b>Q. '.$question_title.'</b></div>';
-                    $options = get_options_by_question_id($conn, $question_id);
-                    if ($options->num_rows > 0) {
-                        echo '<div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">';
-                        while ($option = $options->fetch_assoc()) {
-                            echo '<label class="options">'.$option['option_text'];
-                            echo '<input type="radio" name="question_'.$question_id.'" value="'.$option['correct_option'].'">';
-                            echo '<span class="checkmark"></span></label>';
-                        }
-                        echo '</div>';
+            $totalQuestions = $questions->num_rows;
+            if ($totalQuestions > 0) {
+                $currentQuestion = 1;
+                if (isset($_GET['q'])) {
+                    $currentQuestion = intval($_GET['q']);
+                }
+                if ($currentQuestion <= 0 || $currentQuestion > $totalQuestions) {
+                    $currentQuestion = 1;
+                }
+
+                $questions->data_seek($currentQuestion - 1);
+                $row = $questions->fetch_assoc();
+                $question_id = $row['question_id'];
+                $question_title = $row['question_title'];
+                echo '<div class="question ml-sm-5 pl-sm-5 pt-2">';
+                echo '<div class="py-2 h5"><b>Q. '.$question_title.'</b></div>';
+                $options = get_options_by_question_id($conn, $question_id);
+                if ($options->num_rows > 0) {
+                    echo '<div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">';
+                    while ($option = $options->fetch_assoc()) {
+                        echo '<label class="options">'.$option['option_text'];
+                        echo '<input type="radio" name="question_'.$question_id.'" value="'.$option['correct_option'].'">';
+                        echo '<span class="checkmark"></span></label>';
                     }
                     echo '</div>';
                 }
+                echo '</div>';
+                echo '<div class="d-flex align-items-center pt-3">';
+                if ($currentQuestion > 1) {
+                    echo '<div id="prev">';
+                    echo '<a class="btn btn-primary" href="?category='.$category.'&q='.($currentQuestion - 1).'">Previous</a>';
+                    echo '</div>';
+                }
+                if ($currentQuestion < $totalQuestions) {
+                    echo '<div class="ml-auto mr-sm-5">';
+                    echo '<a class="btn btn-success" href="?category='.$category.'&q='.($currentQuestion + 1).'">Next</a>';
+                    echo '</div>';
+                } else {
+                    echo '<div class="ml-auto mr-sm-5">';
+                    echo '<a class="btn btn-success" href="#">دیدن نمره</a>';
+                    echo '</div>';
+                }
+                echo '</div>';
             } else {
                 echo 'هیچ سوالی برای این دسته بندی وجود ندارد.';
             }
         }
         $conn->close();
         ?>
-        <div class="d-flex align-items-center pt-3">
-            <div id="prev">
-                <button class="btn btn-primary">Previous</button>
-            </div>
-            <div class="ml-auto mr-sm-5">
-                <button class="btn btn-success" id="submit">Next</button>
-            </div>
-        </div>
     </div>
 
     <!-- quiez linksJS -->
@@ -91,8 +108,9 @@
     <script>
         $(document).ready(function() {
             // کد JS برای ارسال فرم و محاسبه نمره
-            $('#submit').on('click', function() {
-                var totalQuestions = <?php echo $questions->num_rows; ?>;
+            $('a[href="#"]').on('click', function(e) {
+                e.preventDefault();
+                var totalQuestions = <?php echo $totalQuestions; ?>;
                 var correctAnswers = 0;
                 for (var i = 1; i <= totalQuestions; i++) {
                     var selectedOption = $("input[name='question_" + i + "']:checked").val();
@@ -100,7 +118,7 @@
                         correctAnswers++;
                     }
                 }
-                var score = correctAnswers+ ' out of ' + totalQuestions;
+                var score = correctAnswers + ' out of ' + totalQuestions;
                 alert('نمره شما: ' + score);
             });
         });
