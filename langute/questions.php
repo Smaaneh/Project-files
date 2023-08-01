ca859f70de8b802b35ba9fe8997a9a8decec91f4
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-08-01 21:10:18 +0430

    update for queiz
    random options and add result

--- langute/questions.php
+++ langute/questions.php
@@ -1,17 +1,11 @@
+<!-- questions.php -->
 <!DOCTYPE html>
 <html>
 <head>
     <!-- metaTAGS -->
     <?php include 'metaTAGS.php';?>
     <!-- Title -->
-    <?php
-    $category = $_GET['category'] ?? '';
-    $title = 'سوالات ';
-    if ($category) {
-        $title .= '(' . $category . ')';
-    }
-    ?>
-    <title><?php echo $title; ?></title>
+    <title>سوالات</title>
     <!-- quiz CSS -->
     <link rel="stylesheet" href="css/film.css">
 </head>
@@ -20,84 +14,85 @@
     <link rel="stylesheet" href="css/quiez.css">
     <div class="container mt-sm-5 my-1">
         <?php
-        // تابع برای اتصال به دیتابیس
-        function connect_db() {
-            $servername = "localhost";
-            $username = "root";
-            $password = "123";
-            $dbname = "langute";
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            if ($conn->connect_error) {
-                die("خطا در اتصال به دیتابیس: " . $conn->connect_error);
+        $servername = "localhost";
+        $username = "root";
+        $password = "123";
+        $dbname = "langute";
+        
+        // اتصال به دیتابیس
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        // بررسی اتصال
+        if ($conn->connect_error) {
+            die("خطا در اتصال به دیتابیس: " . $conn->connect_error);
+        }
+
+        $category = $_GET['category'] ?? '';
+        $currentQuestion = $_GET['q'] ?? 1;
+
+        $sql = "SELECT COUNT(*) AS total FROM questions WHERE category = '$category'";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        $totalQuestions = $row['total'];
+
+        $sql = "SELECT * FROM questions WHERE category = '$category' ORDER BY RAND() LIMIT 1 OFFSET " . ($currentQuestion - 1);
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $question_id = $row['question_id'];
+            $question_title = $row['question_title'];
+        } else {
+            echo 'هیچ سوالی در این دسته‌بندی وجود ندارد.';
+        }
+
+        if (isset($_POST['submit'])) {
+            $selectedOption = $_POST['option'];
+            $sql = "SELECT correct_option FROM options WHERE question_id = $question_id AND correct_option = 1";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0 && $selectedOption == 'correct') {
+                $_SESSION['correct_answers']++;
             }
-            return $conn;
         }
 
-        // تابع برای خواندن سوالات بر اساس دسته بندی
-        function get_questions_by_category($conn, $category) {
-            $sql = "SELECT * FROM questions WHERE category = '$category'";
+        if ($currentQuestion <= $totalQuestions) {
+            echo '<div class="question ml-sm-5 pl-sm-5 pt-2">';
+            echo '<div class="py-2 h5"><b>Q. ' . $question_title . '</b></div>';
+            echo '<form method="post">';
+            $sql = "SELECT * FROM options WHERE question_id = $question_id ORDER BY RAND()";
             $result = $conn->query($sql);
-            return $result;
-        }
-
-        // تابع برای خواندن گزینه‌های یک سوال
-        function get_options_by_question_id($conn, $question_id) {
-            $sql = "SELECT * FROM options WHERE question_id = $question_id";
-            $result = $conn->query($sql);
-            return $result;
-        }
-
-        $conn = connect_db();
-
-        if ($category) {
-            $questions = get_questions_by_category($conn, $category);
-            $totalQuestions = $questions->num_rows;
-            if ($totalQuestions > 0) {
-                $currentQuestion = 1;
-                if (isset($_GET['q'])) {
-                    $currentQuestion = intval($_GET['q']);
-                }
-                if ($currentQuestion <= 0 || $currentQuestion > $totalQuestions) {
-                    $currentQuestion = 1;
-                }
-
-                $questions->data_seek($currentQuestion - 1);
-                $row = $questions->fetch_assoc();
-                $question_id = $row['question_id'];
-                $question_title = $row['question_title'];
-                echo '<div class="question ml-sm-5 pl-sm-5 pt-2">';
-                echo '<div class="py-2 h5"><b>Q. '.$question_title.'</b></div>';
-                $options = get_options_by_question_id($conn, $question_id);
-                if ($options->num_rows > 0) {
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
                     echo '<div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">';
-                    while ($option = $options->fetch_assoc()) {
-                        echo '<label class="options">'.$option['option_text'];
-                        echo '<input type="radio" name="question_'.$question_id.'" value="'.$option['correct_option'].'">';
-                        echo '<span class="checkmark"></span></label>';
-                    }
+                    echo '<label class="options">' . $row['option_text'];
+                    echo '<input type="radio" name="option" value="' . ($row['correct_option'] == 1 ? 'correct' : 'incorrect') . '">';
+                    echo '<span class="checkmark"></span>';
+                    echo '</label>';
                     echo '</div>';
                 }
+            }
+            echo '<div class="d-flex align-items-center pt-3">';
+            if ($currentQuestion > 1) {
+                echo '<div id="prev">';
+                echo '<a class="btn btn-primary" href="?category='.$category.'&q='.($currentQuestion - 1).'">Previous</a>';
                 echo '</div>';
-                echo '<div class="d-flex align-items-center pt-3">';
-                if ($currentQuestion > 1) {
-                    echo '<div id="prev">';
-                    echo '<a class="btn btn-primary" href="?category='.$category.'&q='.($currentQuestion - 1).'">Previous</a>';
-                    echo '</div>';
-                }
-                if ($currentQuestion < $totalQuestions) {
-                    echo '<div class="ml-auto mr-sm-5">';
-                    echo '<a class="btn btn-success" href="?category='.$category.'&q='.($currentQuestion + 1).'">Next</a>';
-                    echo '</div>';
-                } else {
-                    echo '<div class="ml-auto mr-sm-5">';
-                    echo '<a class="btn btn-success" href="#">دیدن نمره</a>';
-                    echo '</div>';
-                }
+            }
+            if ($currentQuestion < $totalQuestions) {
+                echo '<div class="ml-auto mr-sm-5">';
+                echo '<button class="btn btn-success" type="submit" name="submit">Next</button>';
                 echo '</div>';
             } else {
-                echo 'هیچ سوالی برای این دسته بندی وجود ندارد.';
+                echo '<div class="ml-auto mr-sm-5">';
+                echo '<a class="btn btn-success" href="result.php">دیدن نمره</a>';
+                echo '</div>';
+                $_SESSION['total_questions'] = $totalQuestions;
             }
+            echo '</div>';
+            echo '</form>';
+            echo '</div>';
+        } else {
+            echo 'هیچ سوالی در این دسته‌بندی وجود ندارد.';
         }
+
         $conn->close();
         ?>
     </div>
@@ -105,23 +100,5 @@
     <!-- quiez linksJS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            // کد JS برای ارسال فرم و محاسبه نمره
-            $('a[href="#"]').on('click', function(e) {
-                e.preventDefault();
-                var totalQuestions = <?php echo $totalQuestions; ?>;
-                var correctAnswers = 0;
-                for (var i = 1; i <= totalQuestions; i++) {
-                    var selectedOption = $("input[name='question_" + i + "']:checked").val();
-                    if (selectedOption == 1) {
-                        correctAnswers++;
-                    }
-                }
-                var score = correctAnswers + ' out of ' + totalQuestions;
-                alert('نمره شما: ' + score);
-            });
-        });
-    </script>
 </body>
 </html>
---
+++ langute/result.php
@@ -0,0 +1,34 @@
+<!-- result.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- metaTAGS -->
+    <?php include 'metaTAGS.php';?>
+    <!-- Title -->
+    <title>نمره کاربر</title>
+    <!-- quiz CSS -->
+    <link rel="stylesheet" href="css/film.css">
+</head>
+<body>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/quiez.css">
+    <div class="container mt-sm-5 my-1">
+        <?php
+        session_start();
+        $score = $_SESSION['correct_answers'] ?? 0;
+        $totalQuestions = $_SESSION['total_questions'] ?? 0;
+        if ($score) {
+            echo '<div class="py-2 h5">نمره کاربر: ' . $score . ' از ' . $totalQuestions . '</div>';
+        } else {
+            echo 'هیچ نمره‌ای وجود ندارد.';
+        }
+        session_unset();
+        session_destroy();
+        ?>
+        <a class="btn btn-primary mt-3" href="test.php">بازگشت به صفحه آزمون</a>
+    </div>
+
+    <!-- quiez linksJS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
