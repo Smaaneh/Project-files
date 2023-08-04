679b9e219fd3f730c3cc72c03656a46c424dc2ff
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-08-04 13:59:50 +0430

    Update Test.php

--- langute/Test.php
+++ langute/Test.php
@@ -23,9 +23,9 @@
 					</div>
 					<div class="col-lg-6 col-md-6 col-12">
 						<ul class="bread-list">
-							<li><a href="index.html">خانه <i class="fa fa-angle-left"></i></a></li>
+							<li><a href="index.php">خانه <i class="fa fa-angle-left"></i></a></li>
               <li><a href="#">صفحه ها <i class="fa fa-angle-left"></i></a></li>
-							<li class="active"><a href="events.html">آزمون</a></li>
+							<li class="active"><a href="#">آزمون</a></li>
 						</ul>
 					</div>
 				</div>
