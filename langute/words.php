751de7e577c71b933108816d9936719539e1d5eb
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-08-04 15:10:12 +0430

    change to php

---
+++ langute/words.php
@@ -0,0 +1,122 @@
+    <!DOCTYPE html>
+	<html class="no-js" lang="fa">
+    <head>
+		<!-- metaTAGS -->
+		<?php include 'metaTAGS.php';?>
+
+		<!-- Title -->
+		<title>درس های واژگان</title>
+
+		<!-- linksCSS -->
+ 		<?php include 'linksCSS.php';?>
+
+    </head>
+    <body>
+
+  <!-- header-->
+  <?php include 'header.php';?>
+
+		<!-- Breadcrumb -->
+		<div class="breadcrumbs overlay" style="background-image:url('images/breadcrumb-bg.jpg')">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-12">
+						<h2>درس های واژگان</h2>
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<ul class="bread-list">
+							<li><a href="index.html"> خانه <i class="fa fa-angle-left"></i></a></li>
+              <li><a href="courses.html">آموزش و درس ها<i class="fa fa-angle-left"></i></a></li>
+							<li class="active"><a href="words.html"> واژگان <i class="fa fa-angle-left"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/ End Breadcrumb -->
+
+		<!-- Courses -->
+		<section class="courses archive section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/Lesson number/lesson-1.jpg" alt="#">
+								<a href="word1.php" class="btn white primary">شروع یادگیری</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="word1.php">درس یک</a></h4>
+								<p>در این بخش به ساده ترین و مقدماتی ترین کلمات انگلیسی  میپردازیم</p>
+							</div>
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/Lesson number/lesson-2.jpg" alt="#">
+								<a href="word2.php" class="btn white primary">شروع یادگیری</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="word2.php">درس دو</a></h4>
+								<p>کلماتی پیشرفته تر را بیاموزید</p>
+							</div>
+							<!--/ End Course Meta -->
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/Lesson number/lesson-3.jpg" alt="#">
+								<a href="word3.php" class="btn white primary">شروع یادگیری</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="word3.php">درس سوم</a></h4>
+								<p>دانلود کتاب، داستان های کوتاه برای همه</p>
+							</div>
+							<!--/ End Course Meta -->
+						</div>
+						<!--/ End Single Course -->
+					</div>
+					<div class="col-lg-6 col-md-6 col-12">
+						<!-- Single Course -->
+						<div class="single-course">
+							<!-- Course Head -->
+							<div class="course-head overlay">
+								<img src="images/Lesson number/lesson-4.jpg" alt="#">
+								<a href="word4.php" class="btn white primary">شروع یادگیری</a>
+							</div>
+							<!-- Course Body -->
+							<div class="course-body">
+								<h4 class="c-title"><a href="word4.php">درس چهارم</a></h4>
+								<p>در این مرحله شما به یک زبان آموز حرفه ای تبدیل خواهید شد</p>
+							</div>
+						</div>
+						<!--/ End Single Course -->
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/ End Courses -->
+
+    <!-- Clients CSS -->
+    <div class="clients">
+      <div class="container">
+      </div>
+    </div>
+    <!--/ End Clients CSS -->
+   <!-- foother & js links -->
+   <?php include 'foother.php';?>
+    </body>
+</html>
