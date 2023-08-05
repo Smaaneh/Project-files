ca1aad0f5f153506b4ca0b54f7b893e1fae60d38
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-08-06 01:57:54 +0430

    Update word1.php

--- langute/word1.php
+++ langute/word1.php
@@ -58,9 +58,9 @@
         </div>
         <div class="col-lg-6 col-md-6 col-12">
           <ul class="bread-list">
-            <li><a href="index.html">خانه <i class="fa fa-angle-left"></i></a></li>
-            <li><a href="courses.html">آموزش و درس ها<i class="fa fa-angle-left"></i></a></li>
-            <li><a href="words.html">واژگان<i class="fa fa-angle-left"></i></a></li>
+            <li><a href="index.php">خانه <i class="fa fa-angle-left"></i></a></li>
+            <li><a href="courses.php">آموزش و درس ها<i class="fa fa-angle-left"></i></a></li>
+            <li><a href="words.php">واژگان<i class="fa fa-angle-left"></i></a></li>
             <li class="active"><a href="word1.html">درس یک<i class="fa fa-angle-left"></i></a></li>
           </ul>
         </div>
