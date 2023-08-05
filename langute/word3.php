edffe2f61f716ff2ed62c1dc2b098095cb9d0f53
samaneh mohammadi <128172017+Smaaneh@users.noreply.github.com>
2023-08-06 01:54:27 +0430

    Update word3.php

--- langute/word3.php
+++ langute/word3.php
@@ -53,71 +53,8 @@
   </style>
 </head>
 <body>
-  <!-- Header -->
-  <header class="header">
-    <!-- Header Inner -->
-    <div class="header-inner overlay">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-3 col-md-3 col-12">
-            <!-- Logo -->
-            <div class="logo">
-              <a href="index.html"><img src="images/logo.png" alt="#"></a>
-            </div>
-            <!--/ End Logo -->
-            <div class="mobile-menu"></div>
-          </div>
-          <div class="col-lg-9 col-md-9 col-12">
-            <div class="menu-bar" >
-              <nav class="navbar navbar-default">
-                <div class="navbar-collapse">
-                  <!-- Main Menu -->
-                  <ul id="nav" class="nav menu navbar-nav">
-                    <li><a href="contact.html"><i class="fa fa-address-book"></i>ارتباط با ما</a> </li>
-                    <li><a href="events.html"><i class="fa fa-bullhorn"></i>مقالات</a></li>
-                    <li><a href="#"><i class="fa fa-clone"></i>صفحه ها</a>
-                      <ul class="dropdown">
-                        <li><a href="teachers.html">استادان</a></li>
-                                                  <li><a href="Social_learning.html">یادگیری اجتماعی</a></li>
-                                                  <li><a href="Test.html">آزمون</a></li>
-                                                  <li><a href="Progress_tracking.html">ردیابی پیشرفت</a></li>
-                      </ul>
-                    </li>
-                    <li><a href="Learning.html"><i class="fa fa-clone"></i>یادگیری با روش های مختلف</a></li>
-                    <li><a href="courses.html"><i class="fa fa-clone"></i>آموزش ها و درس ها</a></li>
-                    <li class="active"><a href="index.html"><i class="fa fa-home"></i>خانه</a></li>
-                  </ul>
-                  <!-- End Main Menu -->
-                </div>
-              </nav>
-                   <!-- login -->
-            <div class="row">
-                <div class="col-lg-6 col-md-6 col-12">
-                  <div class="login" style="direction:rtl;">
-                    <a href="login.html" class="icon" onclick="login/login.html">
-                      <i class="fa fa-lg fa-user"></i>
-                    </a>
-                  </div>
-      </div>
-            </div>
-
-      <!-- Search Area -->
-              <div class="search-area">
-                <a href="#header" class="icon"><i class="fa fa-search"></i></a>
-                <form class="search-form">
-                  <input type="text" placeholder="جستجو ..." name="search" style="direction:rtl;">
-                  <button value="search " type="submit"><i class="fa fa-search"></i></button>
-                </form>
-              </div>
-              <!-- End Search Area-->
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!--/ End Header Inner -->
-  </header>
-  <!--/ End Header -->
+   <!-- header-->
+   <?php include 'header.php';?>
   <!-- Breadcrumb -->
   <div class="breadcrumbs overlay" style="background-image:url('images/breadcrumb-bg.jpg')">
     <div class="container">
@@ -127,9 +64,9 @@
         </div>
         <div class="col-lg-6 col-md-6 col-12">
           <ul class="bread-list">
-            <li><a href="index.html">خانه <i class="fa fa-angle-left"></i></a></li>
-            <li><a href="courses.html">آموزش و درس ها<i class="fa fa-angle-left"></i></a></li>
-            <li><a href="words.html">واژگان<i class="fa fa-angle-left"></i></a></li>
+            <li><a href="index.php">خانه <i class="fa fa-angle-left"></i></a></li>
+            <li><a href="courses.php">آموزش و درس ها<i class="fa fa-angle-left"></i></a></li>
+            <li><a href="words.php">واژگان<i class="fa fa-angle-left"></i></a></li>
             <li class="active"><a href="#">درس سه<i class="fa fa-angle-left"></i></a></li>
           </ul>
         </div>
@@ -214,103 +151,7 @@ $conn->close();
 
 
   <!--/ End main word-->
-    <!-- Footer -->
-    <footer class="footer section">
-      <!-- Footer Top -->
-      <div class="footer-top overlay">
-        <div class="container" style="text-align: right;">
-          <div class="row">
-            <div class="col-lg-4 col-md-6 col-12">
-              <!-- About -->
-              <div class="single-widget about">
-                <h2>راه های ارتباطی</h2>
-                <ul class="list">
-
-                  <li><i class="fa fa-phone"></i>تلفن : 09130000006 </li>
-                  <li><i class="fa fa-envelope"></i>ایمیل : <a href="mailto:info@youremail.com">langute@gmail.com</a></li>
-                </ul>
-                <!-- Social -->
-                <ul class="social">
-                  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                  <li class="active"><a href="#"><i class="fa fa-facebook"></i></a></li>
-                  <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                  <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                  <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-                </ul>
-                <!-- End Social -->
-              </div>
-              <!--/ End About -->
-            </div>
-            <div class="col-lg-4 col-md-6 col-12">
-              <!-- Useful Links -->
-              <div class="single-widget list">
-                <h2>لینک های مفید</h2>
-                <ul>
-                  <li><i class="fa fa-angle-left"></i><a href="index.html">خانه</a></li>
-                  <li><i class="fa fa-angle-left"></i><a href="contact.html">ارتباط با ما</a></li>
-                  <li><i class="fa fa-angle-left"></i><a href="courses.html">آموزش ها و درس ها</a></li>
-                  <li><i class="fa fa-angle-left"></i><a href="Learning_page.html">یادگیری با روش های مختف</a></li>
-                </ul>
-              </div>
-              <!--/ End Useful Links -->
-            </div>
-            <div class="col-lg-4 col-md-6 col-12">
-              <!-- Useful Links -->
-              <div class="single-widget opening-times">
-                <h2>ساعات پاسخ گویی</h2>
-                <ul class="list">
-                  <li><span>  08am - 10pm  </span><div class="value off">شنبه تا پنجشنبه</div></li>
-                  <li><span>  010am - 06pm  </span><div class="value off">جمعه و روزهای تعطیل</div></li>
-                </ul>
-              </div>
-              <!--/ End Useful Links -->
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-      <!--/ End Footer Top -->
-
-		<!-- Jquery JS-->
-        <script src="js/jquery.min.js"></script>
-        <script src="js/jquery-migrate.min.js"></script>
-		<!-- Colors JS-->
-        <script src="js/colors.js"></script>
-		<!-- Popper JS-->
-        <script src="js/popper.min.js"></script>
-		<!-- Bootstrap JS-->
-        <script src="js/bootstrap.min.js"></script>
-		<!-- Owl Carousel JS-->
-        <script src="js/owl.carousel.min.js"></script>
-		<!-- Jquery Steller JS -->
-		<script src="js/jquery.stellar.min.js"></script>
-		<!-- Final Countdown JS -->
-		<script src="js/finalcountdown.min.js"></script>
-		<!-- Fancy Box JS-->
-		<script src="js/facnybox.min.js"></script>
-		<!-- Magnific Popup JS-->
-		<script src="js/jquery.magnific-popup.min.js"></script>
-		<!-- Circle Progress JS -->
-		<script src="js/circle-progress.min.js"></script>
-		<!-- Nice Select JS -->
-		<script src="js/niceselect.js"></script>
-		<!-- Jquery Steller JS-->
-        <script src="js/jquery.stellar.min.js"></script>
-		<!-- Jquery Steller JS-->
-        <script src="js/cube-portfolio.min.js"></script>
-		<!-- Slick Nav JS-->
-        <script src="js/slicknav.min.js"></script>
-		<!-- Easing JS-->
-        <script src="js/easing.min.js"></script>
-		<!-- Waypoints JS-->
-        <script src="js/waypoints.min.js"></script>
-		<!-- Counter Up JS -->
-		<script src="js/jquery.counterup.min.js"></script>
-		<!-- Scroll Up JS-->
-        <script src="js/jquery.scrollUp.min.js"></script>
-		<!-- Gmaps JS-->
-		<script src="js/gmaps.min.js"></script>
-		<!-- Main JS-->
-        <script src="js/main.js"></script>
+      <!-- foother & js links -->
+      <?php include 'foother.php';?>
     </body>
 </html>
